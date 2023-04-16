0bf7cf6c6e7c49a46bbf6802c9faefb5c080dd79
John Forbes <John.Robert.Forbes@gmail.com>
2023-04-16 10:06:41 +0000

    Updated hak/calculate_duration_bar_width.py.

--- hak/calculate_duration_bar_width.py
+++ hak/calculate_duration_bar_width.py
@@ -1,7 +1,9 @@
 from hak.log_2 import f as log_2
 from math import ceil
 w_max = 23
+
 f = lambda t_ms: 0 if t_ms <= 0 else min(max(0, ceil(log_2(t_ms))), w_max)
+
 t = lambda: all([
   0 == f(-1),
   0 == f(0),
