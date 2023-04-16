4c2b8320767beceee5ceb997a203096007ec5988
John Forbes <John.Robert.Forbes@gmail.com>
2023-04-16 12:35:19 +0000

    Updated hak/function/write_to_file.py.

--- hak/function/write_to_file.py
+++ hak/function/write_to_file.py
@@ -2,6 +2,7 @@ from hak.function.function import Function
 from hak.file.save import f as save
 from hak.file.remove import f as remove
 from hak.file.load import f as load
+
 f = lambda fn, root='.': save(f'{root}/_{fn.name}.py', fn.text)
 _filepath = './_foo.py'
 dn = lambda: remove(_filepath)
