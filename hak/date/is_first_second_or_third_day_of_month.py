edc0478c6bec1517a275f333ede1af9cfac877d9
John Forbes <John.Robert.Forbes@gmail.com>
2023-04-16 05:42:49 +0000

    Updated hak/date/is_first_second_or_third_day_of_month.py.

--- hak/date/is_first_second_or_third_day_of_month.py
+++ hak/date/is_first_second_or_third_day_of_month.py
@@ -1,7 +1,9 @@
 # is_first_second_or_third_day_of_month
 from random import randint
 from datetime import date
+
 def f(δ: date): return 0 < δ.day < 4
+
 def t(): return all([
   f(date(2022, randint(1, 12), 1)),
   f(date(2022, randint(1, 12), 2)),
