40b905d038ba64cca705242d01f009d0f9fe8442
John Forbes <John.Robert.Forbes@gmail.com>
2023-04-16 12:43:29 +0000

    Updated hak/period/contains_day.py.

--- hak/period/contains_day.py
+++ hak/period/contains_day.py
@@ -1,5 +1,7 @@
 from datetime import date
+
 f = lambda day, period: period[0] <= day <= period[1]
+
 t = lambda: all([
   f(date(2022,1,1), (date(2022,1,1), date(2022,1,31))),
   f(date(2022,1,15), (date(2022,1,1), date(2022,1,31))),
