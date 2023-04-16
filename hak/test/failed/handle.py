d406873273e34fbdf8930d749a9615df9378452c
John Forbes <John.Robert.Forbes@gmail.com>
2023-04-16 05:39:19 +0000

    Updated hak/test/failed/handle.py.

--- hak/test/failed/handle.py
+++ hak/test/failed/handle.py
@@ -17,17 +17,20 @@ def f(_Pi_failed, _pi, message, dt_now=None, root='.'):
   dt_now = dt_now or dt.now()
   _Pi_failed.add(_pi)
   save(_Pi_failed, f'{root}/failed.pickle')
-  return False, f"{danger('FAIL')} {warn(_pi)} {message} {dt_now}"
+  return {
+    'result': False,
+    'message': f"{danger('FAIL')} {warn(_pi)} {message} {dt_now}"
+  }
 
 def t():
   up()
-  y = (
-    False,
-    ' '.join([
+  y = {
+    'result': False,
+    'message': ' '.join([
       '\x1b[1;31mFAIL\x1b[0;0m \x1b[0;33mfoo.py\x1b[0;0m foo foo',
       '2022-01-01 00:00:00'
     ])
-  )
+  }
   z = f(set(['failed.py']), 'foo.py', 'foo foo', dt(2022, 1, 1), _dir)
   z_failed_pickle = load_pickle(_failed_pickle_path)
   dn()
