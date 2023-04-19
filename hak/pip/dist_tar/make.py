a85ca6e26bf6be2e155ac076219784dc4993ba39
John Forbes <john.robert.forbes@gmail.com>
2023-04-19 17:21:29 +1000

    Updated hak/pip/dist_tar/make.py.

--- hak/pip/dist_tar/make.py
+++ hak/pip/dist_tar/make.py
@@ -4,21 +4,25 @@ from hak.directory.remove import f as rmdir
 from hak.directory.make import f as mkdir
 from hak.file.save import f as save
 from hak.file.zip.extract import f as extract
+from copy import deepcopy
+from hak.string.print_and_return_false import f as pf
 
-_root = '../temp'
-base = './hak/system/git/commit'
-setup_filepath = _root + '/setup.py'
-
-f = lambda cwd='.': sprun(
-  ['python3', 'setup.py', 'sdist'], cwd=cwd, capture_output=True
-)
+def f(x):
+  x = deepcopy(x)
+  cwd = x['root'] if 'root' in x else '.'
+  return sprun(['python3', 'setup.py', 'sdist'], cwd=cwd, capture_output=True)
 
 def up():
+  _root = '../temp'
   mkdir(_root)
   mkdir(_root+'/hak')
   save(_root+'/hak/__init__.py', '')
-  extract(f'{base}/added_file_pre_commit.zip', '..')
-  save(setup_filepath, "\n".join([
+
+  _base = './hak/system/git/commit'
+  extract(f'{_base}/added_file_pre_commit.zip', '..')
+
+  _setup_filepath = _root + '/setup.py'
+  save(_setup_filepath, "\n".join([
     'from setuptools import setup',
     'from pathlib import Path',
     '',
@@ -41,43 +45,54 @@ def up():
     "  include_package_data=True,",
     ")",
   ]))
+  return {
+    'root': _root,
+    'base': _base,
+    'setup_filepath': _setup_filepath,
+  }
 
-dn = lambda: rmdir(_root)
+dn = lambda x: rmdir(x['root'])
 
 def t():
-  up()
-  z = f(_root)
-  dn()
-  return all([
-    z.args == ['python3', 'setup.py', 'sdist'],
-    z.returncode == 0,
-    z.stdout.decode("utf-8") == '\n'.join([
-      "running sdist",
-      "running egg_info",
-      "creating hak.egg-info",
-      "writing hak.egg-info/PKG-INFO",
-      "writing dependency_links to hak.egg-info/dependency_links.txt",
-      "writing top-level names to hak.egg-info/top_level.txt",
-      "writing manifest file 'hak.egg-info/SOURCES.txt'",
-      "reading manifest file 'hak.egg-info/SOURCES.txt'",
-      "writing manifest file 'hak.egg-info/SOURCES.txt'",
-      "running check",
-      "creating hak-0.0.0",
-      "creating hak-0.0.0/hak",
-      "creating hak-0.0.0/hak.egg-info",
-      "copying files to hak-0.0.0...",
-      "copying README.md -> hak-0.0.0",
-      "copying setup.py -> hak-0.0.0",
-      "copying hak/__init__.py -> hak-0.0.0/hak",
-      "copying hak.egg-info/PKG-INFO -> hak-0.0.0/hak.egg-info",
-      "copying hak.egg-info/SOURCES.txt -> hak-0.0.0/hak.egg-info",
-      "copying hak.egg-info/dependency_links.txt -> hak-0.0.0/hak.egg-info",
-      "copying hak.egg-info/top_level.txt -> hak-0.0.0/hak.egg-info",
-      "Writing hak-0.0.0/setup.cfg",
-      "creating dist",
-      "Creating tar archive",
-      "removing 'hak-0.0.0' (and everything under it)",
-      "",
-    ]),
-    z.stderr.decode("utf-8") == ''
+  x = up()
+  z = f(x)
+  dn(x)
+
+  y_args = ['python3', 'setup.py', 'sdist']
+  if z.args != y_args: return pf(f"z.args != y_args")
+
+  if z.returncode != 0: return pf('z.returncode != 0')
+  
+  y_stdo = '\n'.join([
+    "running sdist",
+    "running egg_info",
+    "creating hak.egg-info",
+    "writing hak.egg-info/PKG-INFO",
+    "writing dependency_links to hak.egg-info/dependency_links.txt",
+    "writing top-level names to hak.egg-info/top_level.txt",
+    "writing manifest file 'hak.egg-info/SOURCES.txt'",
+    "reading manifest file 'hak.egg-info/SOURCES.txt'",
+    "writing manifest file 'hak.egg-info/SOURCES.txt'",
+    "running check",
+    "creating hak-0.0.0",
+    "creating hak-0.0.0/hak",
+    "creating hak-0.0.0/hak.egg-info",
+    "copying files to hak-0.0.0...",
+    "copying README.md -> hak-0.0.0",
+    "copying setup.py -> hak-0.0.0",
+    "copying hak/__init__.py -> hak-0.0.0/hak",
+    "copying hak.egg-info/PKG-INFO -> hak-0.0.0/hak.egg-info",
+    "copying hak.egg-info/SOURCES.txt -> hak-0.0.0/hak.egg-info",
+    "copying hak.egg-info/dependency_links.txt -> hak-0.0.0/hak.egg-info",
+    "copying hak.egg-info/top_level.txt -> hak-0.0.0/hak.egg-info",
+    "Writing hak-0.0.0/setup.cfg",
+    "creating dist",
+    "Creating tar archive",
+    "removing 'hak-0.0.0' (and everything under it)",
+    ""
   ])
+  if z.stdout.decode("utf-8") != y_stdo: return pf('z.stdo != y_stdo')
+  
+  if z.stderr.decode('utf-8') != '': return pf("z.stderr.decode('utf-8') != ''")
+
+  return True
