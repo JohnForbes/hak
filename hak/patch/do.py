6645897eab587f42c1e6077d5bdee5b7e65b725d
John Forbes <john.robert.forbes@gmail.com>
2023-04-19 19:14:58 +1000

    Updated hak/patch/do.py.

--- hak/patch/do.py
+++ hak/patch/do.py
@@ -29,24 +29,29 @@ from hak.string.print_and_return_false import f as pf
 from hak.directory.make import f as mkdir
 from hak.file.load import f as load
 
-temp_root = '.'
-temp_dir_path = f'{temp_root}/hak_test'
-temp_cfg_path = f'{temp_dir_path}/setup.cfg'
-temp_py_path = f'{temp_dir_path}/setup.py'
+from copy import deepcopy
 
 def up():
-  mkdir(temp_dir_path)
+  x = {}
+  x['root'] = f"./hak_test"
+  x['cfg_path'] = f"{x['root']}/setup.cfg"
+  x['py_path'] = f"{x['root']}/setup.py"
+
+  mkdir(x['root'])
 
   _ = sprun(
     ['git', 'clone', 'git@gitlab.com:zereiji/hak_test.git'],
-    cwd=temp_root,
+    cwd=x['root'],
     capture_output=True
   )
 
-  save(temp_cfg_path, "\n".join([
+  x['v'] = {'major': 1, 'minor': 2, 'patch': 3}
+
+  # x['original_setup_cfg_content'] = load(x['cfg_path'])
+  save(x['cfg_path'], "\n".join([
     "[metadata]",
     "name = hak",
-    "version = 1.2.3",
+    f"version = {x['v']['major']}.{x['v']['minor']}.{x['v']['patch']}",
     "author = John Forbes",
     "author_email = john.robert.forbes@gmail.com",
     "description = Function Test Pair Toolbox",
@@ -65,14 +70,16 @@ def up():
     "include_package_data = True",
   ]))
 
-  save(temp_py_path, "\n".join([
+  # x['original_setup_py_content'] = load(x['py_path'])
+  save(x['py_path'], "\n".join([
     "from setuptools import setup",
     "from pathlib import Path",
     "long_description = Path('./README.md').read_text()",
     "",
     "setup(",
     "  name='hak',",
-    "  version='1.2.4',",
+    # "  version='1.2.4',",
+    f"  version='{x['v']['major']}.{x['v']['minor']}.{x['v']['patch']+1}'",
     "  license='MIT',",
     "  description='Function Test Pair Toolbox',",
     "  long_description=long_description,",
@@ -85,32 +92,35 @@ def up():
     "  install_requires=[],",
     ")",
   ]))
+  return x
 
-dn = lambda: rmdirie(temp_dir_path)
+def dn(x):
+  rmdirie(x['root'])
+  # save(x['cfg_path'], x['original_setup_cfg_content'])
+  # save(x['py_path'], x['original_setup_py_content'])
 
 def f(x):
+  x = deepcopy(x)
   z = {}
   z['v'] = x['v'] if 'v' in x else None
-  z['root'] = x['root'] if 'root' in x else '.'
+  z['root'] = x['root'] # if 'root' in x else '.'
   z['cfg_path'] = f'{z["root"]}/setup.cfg'
   z['py_path'] = f'{z["root"]}/setup.py'
 
   z['v'] = z['v'] or get_pip_version('hak')
   z['v']['patch'] += 1
 
-  update_setup_cfg(z)
-  update_setup_py(z)
+  update_setup_cfg({'v': z['v'], 'filename': z['cfg_path']})
+  update_setup_py({'v': z['v'], 'filename': z['py_path']})
   remove_dist_tar(x)
   generate_new_dist_tar(x)
   # add_to_git(cwd=_root, cap_out=True)
-  z['upload_result'] = start_upload(x)
+  if 'local_test_only' not in x: z['upload_result'] = start_upload(x)
   return z
 
 def t():
-  x = {
-    'v': {'major': 1, 'minor': 2, 'patch': 3},
-    'root': temp_dir_path
-  }
+  x = up()
+  x['local_test_only'] = True
   y = {
     'v': {'major': 1, 'minor': 2, 'patch': 4},
     'cfg_path': './hak_test/setup.cfg',
@@ -121,21 +131,12 @@ def t():
       'stdout': b'Uploading distributions to',
       'stderr': b''
     },
-    'root': temp_dir_path
+    'root': x['root']
   }
-  up()
   z = f(x)
-  dn()
+  dn(x)
 
   if z is None: return pf([f"z is None", f'x: {x}', f'y: {y}', f'z: {z}'])
-
-  # if y != z:
-  #   print('y != z')
-  #   for k in (set(y.keys()) | set(z.keys())):
-  #     if not y[k] == z[k]:
-  #       return pf([f"y[{k}] == z[{k}]", f'y[{k}]: {y[k]}', f'z[{k}]: {z[k]}'])
-    
-  #   return False
 
   if y['v'] != z['v']:
     return pf([f"y['v'] != z['v']", f'x: {x}', f'y: {y}', f'z: {z}'])
@@ -145,94 +146,101 @@ def t():
       if not y[k] == z[k]:
         return pf([f"y[{k}] == z[{k}]", f'x: {x}', f'y: {y}', f'z: {z}'])
   
-  if not (
-    len(y['upload_result']['args']) ==
-    len(z['upload_result']['args']) ==
-    7
-  ):
-    return pf([
-      ' == '.join([
-        "not (len(y['upload_result']['args'])",
-        "len(z['upload_result']['args'])",
-        "5)"
-      ]),
-      f"y['upload_result']['args']: {y['upload_result']['args']}",
-      f"z['upload_result']['args']: {z['upload_result']['args']}"
-    ])
+  if not 'local_test_only':
+    if not (
+      len(y['upload_result']['args']) ==
+      len(z['upload_result']['args']) ==
+      7
+    ):
+      return pf([
+        ' == '.join([
+          "not (len(y['upload_result']['args'])",
+          "len(z['upload_result']['args'])",
+          "7)"
+        ]),
+        f"y['upload_result']['args']: {y['upload_result']['args']}",
+        f"z['upload_result']['args']: {z['upload_result']['args']}"
+      ])
 
-  if not (
-    y['upload_result']['args'][0] ==
-    z['upload_result']['args'][0] ==
-    'twine'
-  ):
-    return pf(' == '.join([
-      "not (y['upload_result']['args'][0]",
-      "z['upload_result']['args'][0]",
-      "'twine')"
-    ]))
+    if not (
+      y['upload_result']['args'][0] ==
+      z['upload_result']['args'][0] ==
+      'twine'
+    ):
+      return pf(' == '.join([
+        "not (y['upload_result']['args'][0]",
+        "z['upload_result']['args'][0]",
+        "'twine')"
+      ]))
 
-  if not (
-    y['upload_result']['args'][1] ==
-    z['upload_result']['args'][1] ==
-    'upload'
-  ):
-    return pf(' == '.join([
-      "not (y['upload_result']['args'][1]",
-      "z['upload_result']['args'][1]",
-      "'upload')"
-    ]))
+    if not (
+      y['upload_result']['args'][1] ==
+      z['upload_result']['args'][1] ==
+      'upload'
+    ):
+      return pf(' == '.join([
+        "not (y['upload_result']['args'][1]",
+        "z['upload_result']['args'][1]",
+        "'upload')"
+      ]))
 
-  if not (
-    y['upload_result']['args'][2] ==
-    z['upload_result']['args'][2] ==
-    'dist/*'
-  ):
-    return pf(' == '.join([
-      "not (y['upload_result']['args'][2]",
-      "z['upload_result']['args'][2]",
-      "'dist/*')"
-    ]))
+    if not (
+      y['upload_result']['args'][2] ==
+      z['upload_result']['args'][2] ==
+      'dist/*'
+    ):
+      return pf(' == '.join([
+        "not (y['upload_result']['args'][2]",
+        "z['upload_result']['args'][2]",
+        "'dist/*')"
+      ]))
 
-  if not (
-    z['upload_result']['args'][4] ==
-    load('username.secret').split('\n')[0]
-  ):
-    return pf(' == '.join([
-      "not (z['upload_result']['args'][4]",
-      "load('username.secret').split('\n')[0])"
-    ]))
+    if not (
+      z['upload_result']['args'][4] ==
+      load('username.secret').split('\n')[0]
+    ):
+      return pf(' == '.join([
+        "not (z['upload_result']['args'][4]",
+        "load('username.secret').split('\n')[0])"
+      ]))
 
-  if not (
-    z['upload_result']['args'][6] ==
-    load('password.secret').split('\n')[0]
-  ):
-    return pf(' == '.join([
-      "not (z['upload_result']['args'][6]",
-      "load('password.secret').split('\n')[0])"
-    ]))
+    if not (
+      z['upload_result']['args'][6] ==
+      load('password.secret').split('\n')[0]
+    ):
+      return pf(' == '.join([
+        "not (z['upload_result']['args'][6]",
+        "load('password.secret').split('\n')[0])"
+      ]))
 
-  if not (
-    y['upload_result']['returncode'] ==
-    z['upload_result']['returncode'] ==
-    0
-  ):
-    return pf(' == '.join([
-      "not y['upload_result']['returncode']",
-      "z['upload_result']['returncode']",
-      "0"
-    ]))
+    if not (
+      y['upload_result']['returncode'] ==
+      z['upload_result']['returncode'] ==
+      0
+    ):
+      return pf(' == '.join([
+        "not y['upload_result']['returncode']",
+        "z['upload_result']['returncode']",
+        "0"
+      ]))
 
-  if not (y['upload_result']['stderr'] == z['upload_result']['stderr'] == b''):
-    return pf(' == '.join([
-      "not y['upload_result']['stderr'] == z['upload_result']['stderr'] == b''"
-    ]))
+    if not (
+      y['upload_result']['stderr'] ==
+      z['upload_result']['stderr'] ==
+      b''
+    ):
+      return pf(' == '.join(' == '.join(
+        "not y['upload_result']['stderr']",
+        "z['upload_result']['stderr']",
+        "b''"
+      )))
 
-  if y['upload_result']['stdout'] not in z['upload_result']['stdout']:
-    return pf([
-      "y['upload_result']['stdout'] not in z['upload_result']['stdout']",
-      f"y['upload_result']['stdout']: {y['upload_result']['stdout']}",
-      f"z['upload_result']['stdout']: {z['upload_result']['stdout']}"
-    ])
+    if y['upload_result']['stdout'] not in z['upload_result']['stdout']:
+      return pf([
+        "y['upload_result']['stdout'] not in z['upload_result']['stdout']",
+        f"y['upload_result']['stdout']: {y['upload_result']['stdout']}",
+        f"z['upload_result']['stdout']: {z['upload_result']['stdout']}"
+      ])
 
   if not t_get_pip_version():
     return pf([f"t_get_pip_version()", f'x: {x}', f'y: {y}', f'z: {z}'])
@@ -259,9 +267,5 @@ def t():
 
   if not t_start_upload():
     return pf([f"t_start_upload()", f'x: {x}', f'y: {y}', f'z: {z}'])
-
-  # print(f'x: {x}')
-  # print(f'y: {y}')
-  # print(f'z: {z}')
 
   return True
