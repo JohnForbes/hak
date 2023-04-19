378ad8616129f5df9ced0736cbce8df347498de0
John Forbes <john.robert.forbes@gmail.com>
2023-04-19 15:00:54 +1000

    Updated hak/string/format.py.

--- hak/string/format.py
+++ hak/string/format.py
@@ -1,23 +1,23 @@
 # ignore_overlength_linesontain version
-from hak.string.append.new_line import f as append_new_line
-from hak.string.insert.new_line_after_last_import_line import f as place_blank_line_after_last_import_line
-from hak.string.insert.new_line_before_comment import f as insert_nl_before_comment
-from hak.string.insert.new_line_before_def import f as insert_nl_before_def
-from hak.string.insert.new_line_before_lambda import f as add_nl_before_lambda_methods
-from hak.string.insert.new_line_before_lambda import f as insert_nl_before_lambda
-from hak.string.insert.new_line_before_if_main import f as insert_nl_before_if_main
-from hak.string.remove.empty_line_spaces import f as remove_empty_line_spaces
-from hak.string.remove.extra_new_line_following_class_definition import f as remove_gap_following_class_definition
-from hak.string.remove.first_new_line_if_starts_with_new_line import f as remove_first_nl_if_file_starts_with_newline
-from hak.string.replace.double_new_line_with_single_new_line import f as replace_double_nl_with_single_new_line
-from hak.string.replace.single_new_line_with_empty_string import f as replace_single_nl_content_with_empty_string
-from hak.string.replace.triple_new_line_with_double_new_line import f as replace_triple_newlines_with_double_newlines
-from hak.string.insert.new_line_before_new_line_and_cea_ import f as insert_nl_before_nl_and_cea_
-from hak.string.indent.run_if_class_method import f as indent_if_is_run_method
-from hak.string.remove.whitespace_between_newlines import f as remove_whitespace_between_newlines
-from hak.string.colour.bright.yellow import f as yellow
-from hak.string.colour.bright.cyan import f as cy
-from hak.string.colour.bright.magenta import f as magenta
+from .append.new_line import f as append_new_line
+from .insert.new_line_after_last_import_line import f as place_blank_line_after_last_import_line
+from .insert.new_line_before_comment import f as insert_nl_before_comment
+from .insert.new_line_before_def import f as insert_nl_before_def
+from .insert.new_line_before_lambda import f as add_nl_before_lambda_methods
+from .insert.new_line_before_lambda import f as insert_nl_before_lambda
+from .insert.new_line_before_if_main import f as insert_nl_before_if_main
+from .remove.empty_line_spaces import f as remove_empty_line_spaces
+from .remove.extra_new_line_following_class_definition import f as remove_gap_following_class_definition
+from .remove.first_new_line_if_starts_with_new_line import f as remove_first_nl_if_file_starts_with_newline
+from .replace.double_new_line_with_single_new_line import f as replace_double_nl_with_single_new_line
+from .replace.single_new_line_with_empty_string import f as replace_single_nl_content_with_empty_string
+from .replace.triple_new_line_with_double_new_line import f as replace_triple_newlines_with_double_newlines
+from .insert.new_line_before_new_line_and_cea_ import f as insert_nl_before_nl_and_cea_
+from .indent.run_if_class_method import f as indent_if_is_run_method
+from .remove.whitespace_between_newlines import f as remove_whitespace_between_newlines
+from .colour.bright.yellow import f as yellow
+from .colour.bright.cyan import f as cy
+from .colour.bright.magenta import f as magenta
 from hak.list.strings.compare import f as compare_strings
 
 def f(x: str):
@@ -148,21 +148,12 @@ def t():
   ])
   test_result = y == z
   if not test_result:
-    print('x:')
-    print('-'*80)
-    print(yellow(x))
-    print('-'*80)
-    print()
-    print('z:')
-    print('-'*80)
-    print(cy(z))
-    print('-'*80)
-    print()
-    print('y:')
-    print('-'*80)
-    print(magenta(y))
-    print('-'*80)
-    print()
+    for _ in [
+      'x:', '-'*80, yellow(x), '-'*80, '',
+      'y:', '-'*80, magenta(y), '-'*80, '',
+      'z:', '-'*80, cy(z), '-'*80, '',
+    ]:
+      print(_)
     
     result = compare_strings(y, z)
     if len(result) == 3:
