9bfea32b4270d33c1c1668b0f86367f23abb69ea
John Forbes <john.robert.forbes@gmail.com>
2023-04-19 13:16:28 +1000

    Renamed hak/directory/is_module.pz -> hak/directory/is_module.py.  Modified setup.cfg.  Modified setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = Path("./README.md").read_text()
 
 setup(
   name='hak',
-  version='0.0.1',
+  version='0.0.2',
   license='MIT',
   description='Function Test Pair Toolbox',
   long_description=long_description,
