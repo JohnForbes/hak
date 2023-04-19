9c11090e223920e4769162fc004be5af26a42824
John Forbes <john.robert.forbes@gmail.com>
2023-04-19 14:44:45 +1000

    Updated hak/pip/upload.py.

--- hak/pip/upload.py
+++ hak/pip/upload.py
@@ -3,15 +3,16 @@ from hak.fake.subprocess.run import f as fake_sprun
 from hak.directory.make import f as mkdirine
 from hak.directory.remove import f as rmdirie
 from hak.file.load import f as load
+from hak.string.print_and_return_false import f as pf
 
 _dir = '../start_upload'
 
-def up(): return mkdirine(_dir)
+up = lambda: mkdirine(_dir)
 
-def dn(): return rmdirie(_dir)
+dn = lambda: rmdirie(_dir)
 
-_username = load('username.secret')
-_password = load('password.secret')
+_username = load('username.secret').split('\n')[0]
+_password = load('password.secret').split('\n')[0]
 
 def f(sprun=sprun, cwd='.', capture_output=True):
   return sprun(
@@ -23,16 +24,18 @@ def f(sprun=sprun, cwd='.', capture_output=True):
 def t():
   up()
   z = f(fake_sprun, cwd=_dir)
-  result = all([
-    _password in z.args,
-    _username in z.args,
-    'twine' in z.args,
-    'upload' in z.args,
-    'dist/*' in z.args,
-    '-u' in z.args,
-    '-p' in z.args,
-    z.returncode == 0,
-    'Uploading distributions to https://upload.pypi.org' in z.stdout.decode()
-  ])
   dn()
-  return result
+  if not _password in z.args: return pf([
+    "_password in z.args",
+    f'z.args: {z.args}'
+  ])
+  if not _username in z.args: return pf("_username in z.args")
+  if not 'twine' in z.args: return pf("'twine' in z.args")
+  if not 'upload' in z.args: return pf("'upload' in z.args")
+  if not 'dist/*' in z.args: return pf("'dist/*' in z.args")
+  if not '-u' in z.args: return pf("'-u' in z.args")
+  if not '-p' in z.args: return pf("'-p' in z.args")
+  if not z.returncode == 0: return pf("z.returncode == 0")
+  if not 'distributions to https://upload.pypi.org' in z.stdout.decode():
+    return pf("'distributions to https://upload.pypi.org' in z.stdout.decode()")
+  return True
