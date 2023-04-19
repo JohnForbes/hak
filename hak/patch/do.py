a787c635301072b03718572485bda726e0ea0076
John Forbes <john.robert.forbes@gmail.com>
2023-04-19 17:12:14 +1000

    Updated hak/patch/do.py.

--- hak/patch/do.py
+++ hak/patch/do.py
@@ -1,30 +1,32 @@
 from hak.pip.version.get import f as get_pip_version
-from hak.pip.version.get import t as test_get_pip_version
+from hak.pip.version.get import t as t_get_pip_version
 
 from hak.setup.cfg.update import f as update_setup_cfg
-from hak.setup.cfg.update import t as test_update_setup_cfg
+from hak.setup.cfg.update import t as t_update_setup_cfg
 
 from hak.setup.py.update import f as update_setup_py
-from hak.setup.py.update import t as test_update_setup_py
+from hak.setup.py.update import t as t_update_setup_py
 
 from hak.pip.dist_tar.make import f as generate_new_dist_tar
-from hak.pip.dist_tar.make import t as test_generate_new_dist_tar
+from hak.pip.dist_tar.make import t as t_generate_new_dist_tar
 
 from hak.system.git.commit.run import f as add_to_git
-from hak.system.git.commit.run import t as test_add_to_git
+from hak.system.git.commit.run import t as t_add_to_git
 
 from hak.pip.upload import f as start_upload
-from hak.pip.upload import t as test_start_upload
+from hak.pip.upload import t as t_start_upload
 
 from hak.directory.remove import f as rmdirie
 
 from hak.file.save import f as save
 
 from hak.pip.dist_tar.remove import f as remove_dist_tar
-from hak.pip.dist_tar.remove import t as test_remove_dist_tar
+from hak.pip.dist_tar.remove import t as t_remove_dist_tar
 
 from subprocess import run as sprun
 from hak.string.print_and_return_false import f as pf
+
+from hak.directory.make import f as mkdir
 
 temp_root = '..'
 temp_dir_path = f'{temp_root}/hak_test'
@@ -32,6 +34,8 @@ temp_cfg_path = f'{temp_dir_path}/setup.cfg'
 temp_py_path = f'{temp_dir_path}/setup.py'
 
 def up():
+  mkdir(temp_dir_path)
+
   _ = sprun(
     ['git', 'clone', 'git@gitlab.com:zereiji/hak_test.git'],
     cwd=temp_root,
@@ -83,48 +87,97 @@ def up():
 
 dn = lambda: rmdirie(temp_dir_path)
 
+def f(x):
+  z = {}
+  z['v'] = x['v'] if 'v' in x else None
+  z['root'] = x['root'] if 'root' in x else '.'
+  z['cfg_path'] = f'{z["root"]}/setup.cfg'
+  z['py_path'] = f'{z["root"]}/setup.py'
+
+  z['v'] = z['v'] or get_pip_version('hak')
+  z['v']['patch'] += 1
+
+  update_setup_cfg(z)
+  update_setup_py(z)
+  # remove_dist_tar()
+  generate_new_dist_tar()
+  # add_to_git(cwd=_root, cap_out=True)
+  # upload_result = start_upload()
+  z['upload_result'] = True
+
+  return z
+  # return {
+  #   'v': v,
+  #   'cfg_path': cfg_path,
+  #   'py_path': py_path,
+  #   'upload_result': upload_result
+  # } 
+
 def t():
-  up()
-  z = f(v={'major': 1, 'minor': 2, 'patch': 3}, _root=temp_dir_path)
+  x = {
+    'v': {'major': 1, 'minor': 2, 'patch': 3},
+    'root': temp_dir_path
+  }
   y = {
     'v': {'major': 1, 'minor': 2, 'patch': 4},
     'cfg_path': '../hak_test/setup.cfg',
-    'py_path': '../hak_test/setup.py'
+    'py_path': '../hak_test/setup.py',
+    'upload_result': True,
+    'root': temp_dir_path
   }
-  result = all([
-    z['v'] == y['v'],
-    z['cfg_path'] == y['cfg_path'],
-    z['py_path'] == y['py_path'],
-    test_get_pip_version,
-    test_update_setup_cfg,
-    test_update_setup_py,
-    test_remove_dist_tar,
-    test_generate_new_dist_tar,
-    test_add_to_git,
-    test_start_upload,
-  ])
+  up()
+  z = f(x)
   dn()
-  return result or pf([
-    f'z: {z}',
-    f'y: {y}'
-  ])
 
-def f(v=None, _root='.'):
-  cfg_path = f'{_root}/setup.cfg'
-  py_path = f'{_root}/setup.py'
+  if z is None: return pf([f"z is None", f'x: {x}', f'y: {y}', f'z: {z}'])
 
-  v = v or get_pip_version('hak')
-  v['patch'] += 1
-  update_setup_cfg(v, cfg_path)
-  update_setup_py(v, py_path)
-  remove_dist_tar()
-  generate_new_dist_tar(_root)
-  add_to_git(cwd=_root, cap_out=True)
-  upload_result = start_upload()
+  if y != z:
+    y_keys = set(y.keys())
+    z_keys = set(z.keys())
+    return pf([
+      'y != z',
+      f'y_keys - z_keys: {y_keys - z_keys}',
+      f'z_keys - y_keys: {z_keys - y_keys}',
+      f'x: {x}',
+      f'y: {y}',
+      f'z: {z}'
+    ])
 
-  return {
-    'v': v,
-    'cfg_path': cfg_path,
-    'py_path': py_path,
-    'upload_result': upload_result
-  } 
+  if y['v'] != z['v']:
+    return pf([f"y['v'] != z['v']", f'x: {x}', f'y: {y}', f'z: {z}'])
+  
+  for k in (set(y.keys()) | set(z.keys())):
+    if not y[k] == z[k]:
+      return pf([
+        f"y[{k}] == z[{k}]",
+        f'x: {x}',
+        f'y: {y}',
+        f'z: {z}'
+      ])
+
+  if not t_get_pip_version():
+    return pf([f"t_get_pip_version()", f'x: {x}', f'y: {y}', f'z: {z}'])
+
+  if not t_update_setup_cfg():
+    return pf([f"t_update_setup_cfg()", f'x: {x}', f'y: {y}', f'z: {z}'])
+
+  if not t_update_setup_py():
+    return pf([f"t_update_setup_py()", f'x: {x}', f'y: {y}', f'z: {z}'])
+
+  if not t_remove_dist_tar():
+    return pf([f"t_remove_dist_tar()", f'x: {x}', f'y: {y}', f'z: {z}'])
+
+  if not t_generate_new_dist_tar():
+    return pf([f"t_generate_new_dist_tar()", f'x: {x}', f'y: {y}', f'z: {z}'])
+
+  if not t_add_to_git():
+    return pf([f"t_add_to_git()", f'x: {x}', f'y: {y}', f'z: {z}'])
+
+  if not t_start_upload():
+    return pf([f"t_start_upload()", f'x: {x}', f'y: {y}', f'z: {z}'])
+
+  # print(f'x: {x}')
+  # print(f'y: {y}')
+  # print(f'z: {z}')
+
+  return True
