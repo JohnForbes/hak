e40666e013e80e9347639639bf38c7bd20aa55b3
John Forbes <john.robert.forbes@gmail.com>
2023-07-06 18:51:15 +1000

    Removed hak/string/remove/first_new_line_if_starts_with_new_line.py.

--- hak/string/remove/first_new_line_if_starts_with_new_line.py
+++
@@ -1,8 +0,0 @@
-f = lambda k: k.replace('\n', '', 1) if k.startswith('\n') else k
-
-t = lambda: all([
-  'abc' == f('abc'),
-  'abc\nxyz' == f('abc\nxyz'),
-  'abc\nxyz' == f('\nabc\nxyz'),
-  '\nabc\nxyz' == f('\n\nabc\nxyz'),
-])
