a5194ec6a0186eabf359fd7d7f19605cde2f8f16
John Forbes <john.robert.forbes@gmail.com>
2023-07-06 17:06:00 +1000

    Removed hak/function/function.py.

--- hak/function/function.py
+++
@@ -1,6 +0,0 @@
-class Function:
-  def __init__(self, name, text): self.name = name; self.text = text
-
-f = lambda name, text: Function(name, text)
-
-def t(): fn = f('abc', 'xyz'); return all([fn.name == 'abc', fn.text == 'xyz'])
