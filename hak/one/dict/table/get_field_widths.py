3e617e6239fd955a641f7433640ae643f0915c90
John Forbes <john.robert.forbes@gmail.com>
2023-07-07 16:20:28 +1000

    Updated hak/one/dict/table/get_field_widths.py.

--- hak/one/dict/table/get_field_widths.py
+++ hak/one/dict/table/get_field_widths.py
@@ -1,32 +1,23 @@
-from copy import deepcopy
 from hak.one.string.print_and_return_false import f as pf
-from hak.one.dict.cell.get_width import f as get_width
+from hak.one.dict.cell.get_width import f as get_w
 
-def f(x):
-  field_names = deepcopy(x['field_names'])
-  records = x['records']
-  return {
-    field_name: max([
-      get_width(
-        {
-          'value': record[field_name] if field_name in record else None,
-          'field_name': field_name
-        }
-      )
-      for record in records
-    ])
-    for field_name in field_names
-  }
+f = lambda x: {
+  k: max([
+    get_w({'value': r[k] if k in r else None, 'field_name': k})
+    for r in x['records']
+  ])
+  for k in x['field_names']
+}
 
 def t():
   x = {
-    'field_names': ['a', 'b', 'c', 'd', 'e'],
+    'field_names': list('abcde'),
     'records': [
-      {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4},
-      {'a': 5, 'b': 6, 'c': 7, 'd': 8, 'e': 9},
+      {'a':  0, 'b':  1, 'c':  2, 'd':  3, 'e':  4},
+      {'a':  5, 'b':  6, 'c':  7, 'd':  8, 'e':  9},
       {'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14},
     ]
   }
-  y = {'a': 2, 'b': 2, 'c': 2, 'd': 2, 'e': 2}
+  y = {k: 2 for k in list('abcde')}
   z = f(x)
   return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
