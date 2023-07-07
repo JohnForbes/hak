56bb210f5d8c5a0a91a9b031e81ce4515ade027d
John Forbes <john.robert.forbes@gmail.com>
2023-07-07 16:15:45 +1000

    Updated hak/one/dict/custom_order/apply.py.

--- hak/one/dict/custom_order/apply.py
+++ hak/one/dict/custom_order/apply.py
@@ -4,17 +4,11 @@ from hak.one.string.print_and_return_false import f as pf
 # apply_custom_order
 def f(x):
   field_order = deepcopy(x['field_order'])
-  field_names = deepcopy(x['field_names'])
-  for field_name in field_order:
-    if field_name in field_names:
-      field_names.remove(field_name)
+  field_names = [n for n in x['field_names'] if n not in field_order]
   return field_order + field_names
 
 def t():
-  x = {
-    'field_order': ['c', 'b', 'a'],
-    'field_names': ['a', 'b', 'c', 'd', 'e', 'f'],
-  }
+  x = {'field_order': list('cba'), 'field_names': list('abcdef')}
   y = ['c', 'b', 'a', 'd', 'e', 'f']
   z = f(x)
   return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
