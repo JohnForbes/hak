13fd297638e20ef5532e00bb12ba4cb3c12a66f7
John Forbes <john.robert.forbes@gmail.com>
2023-07-10 23:16:52 +1000

    Updated hak/many/tuples/to_dict.py.

--- hak/many/tuples/to_dict.py
+++ hak/many/tuples/to_dict.py
@@ -1,4 +1,4 @@
-from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 # convert_list_of_tuples_to_dict
 def f(x):
@@ -10,4 +10,4 @@ def t():
   x = [('a', 1), ('b', 2), ('a', 3), ('b', 4)]
   y = {'a': 4, 'b': 6}
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
