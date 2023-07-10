ba4f7bc4575866644de30a85643de43dc230f826
John Forbes <john.robert.forbes@gmail.com>
2023-07-11 00:54:37 +1000

    Updated hak/one/dict/period/month/get_end_date.py.

--- hak/one/dict/period/month/get_end_date.py
+++ hak/one/dict/period/month/get_end_date.py
@@ -1,19 +1,49 @@
 from datetime import date
 from datetime import timedelta
 from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 # get_ω_date_of_month
 f = lambda x: (
-  date(year=x['year'], month=x['month_number']+1, day=1) - timedelta(days=1)
-  if x['month_number'] < 12 else
-  date(year=x['year']+1, month=1, day=1) - timedelta(days=1)
+  date(
+    year=x['month']['year'],
+    month=x['month']['number']+1,
+    day=1
+  ) - timedelta(days=1)
+  if x['month']['number'] < 12 else
+  date(year=x['month']['year']+1, month=1, day=1) - timedelta(days=1)
 )
 
-t_0 = lambda: date(2016,  5, 31) == f({'year': 2016, 'month_number':  5})
-t_1 = lambda: date(2017,  6, 30) == f({'year': 2017, 'month_number':  6})
-t_2 = lambda: date(2020,  2, 29) == f({'year': 2020, 'month_number':  2})
-t_3 = lambda: date(2021,  2, 28) == f({'year': 2021, 'month_number':  2})
-t_4 = lambda: date(2021, 12, 31) == f({'year': 2021, 'month_number': 12})
+def t_0():
+  x = {'month': {'year': 2016, 'number':  5}}
+  y = date(2016,  5, 31)
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_1():
+  x = {'month': {'year': 2017, 'number':  6}}
+  y = date(2017,  6, 30)
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_2():
+  x = {'month': {'year': 2020, 'number':  2}}
+  y = date(2020,  2, 29)
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_3():
+  x = {'month': {'year': 2021, 'number':  2}}
+  y = date(2021,  2, 28)
+  z = f(x)
+  return pxyz(x, y, z)
+
+def t_4():
+  x = {'month': {'year': 2021, 'number': 12}}
+  y = date(2021, 12, 31)
+  z = f(x)
+  return pxyz(x, y, z)
+
 
 def t():
   if not t_0(): return pf('!t_0')
