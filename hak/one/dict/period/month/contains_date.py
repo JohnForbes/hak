041abad92f3dba3a2bd4e12d07b5671ca4160feb
John Forbes <john.robert.forbes@gmail.com>
2023-07-11 00:30:26 +1000

    Updated hak/one/dict/period/month/contains_date.py.

--- hak/one/dict/period/month/contains_date.py
+++ hak/one/dict/period/month/contains_date.py
@@ -1,6 +1,6 @@
 from datetime import date
-from hak.one.dict.period.month.get_first_date import f as get_α_d
-from hak.one.dict.period.month.get_last_date import f as get_ω_d
+from hak.one.dict.period.month.get_start_date import f as get_α_d
+from hak.one.dict.period.month.get_end_date import f as get_ω_d
 from hak.one.string.print_and_return_false import f as pf
 
 # contains_date
