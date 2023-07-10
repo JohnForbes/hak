2b3528eb4853b535ca59e210978d8d2ea41ebae7
John Forbes <john.robert.forbes@gmail.com>
2023-07-10 23:19:31 +1000

    Updated hak/one/dict/period/contains_date.py.

--- hak/one/dict/period/contains_date.py
+++ hak/one/dict/period/contains_date.py
@@ -1,8 +1,10 @@
 from datetime import date
+
 from hak.one.dict.period.financial_year.contains_date import f as d_in_fy
 from hak.one.dict.period.financial_year.make import f as mkfy
 from hak.one.dict.period.month.contains_date import f as d_in_m
 from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 # contains_d
 f = lambda x: (d_in_m if 'month' in x else d_in_fy)(x)
@@ -11,13 +13,13 @@ def t_fy_true():
   x = {'financial_year': mkfy({'start_year': 2022}), 'date': date(2022, 7, 5)}
   y = True
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_fy_false():
   x = {'financial_year': mkfy({'start_year': 2022}), 'date': date(2022, 6, 5)}
   y = False
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_month():
   x = {'month': {'year': 2022, 'month_number': 1}, 'date': date(2022, 1, 10)}
