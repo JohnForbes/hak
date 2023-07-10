a669c7d43510da8ccffca307092a6a251a7f6de7
John Forbes <john.robert.forbes@gmail.com>
2023-07-10 23:25:04 +1000

    Updated hak/one/string/date/to_date.py.

--- hak/one/string/date/to_date.py
+++ hak/one/string/date/to_date.py
@@ -1,11 +1,12 @@
 from datetime import date
 
-from hak.one.string.date.separator.get import f as get_separator
-from hak.one.string.print_and_return_false import f as pf
+from hak.many.strings.date_pieces.get import f as get_bag
 from hak.many.strings.date_pieces.separate_day import f as separate_day
 from hak.many.strings.date_pieces.separate_year import f as separate_year
-from hak.many.strings.date_pieces.get import f as get_bag
+from hak.one.string.date.separator.get import f as get_separator
 from hak.one.string.month.to_month_number import f as to_month_number
+from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 # src.string.to_date
 def f(x, date_string_format=None):
@@ -29,33 +30,33 @@ def t_0():
   x = '19 Nov 2021'
   y = date(2021, 11, 19)
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_1():
   x = '2021-11-19'
   y = date(2021, 11, 19)
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_2():
   x = '28/03/2022'
   y = date(2022, 3, 28)
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_3():
   x = '5/04/2022'
   x_date_string_format = {'year_index': 2, 'month_index': 1, 'day_index': 0}
   y = date(2022, 4, 5)
   z = f(x, x_date_string_format)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_4():
   x = '2022-05-06'
   x_date_string_format = {'year_index': 0, 'month_index': 1, 'day_index': 2}
   y = date(2022, 5, 6)
   z = f(x, x_date_string_format)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 t_5 = lambda: f('2016-11-14') == date(2016, 11, 14)
 
