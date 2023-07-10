d2e65d402a3ffd5d18eb5d0786663a88de1c18f3
John Forbes <john.robert.forbes@gmail.com>
2023-07-10 23:09:09 +1000

    Updated hak/many/strings/date_pieces/get.py.

--- hak/many/strings/date_pieces/get.py
+++ hak/many/strings/date_pieces/get.py
@@ -1,5 +1,6 @@
-from hak.one.string.print_and_return_false import f as pf
 from hak.one.string.date.separator.get import f as get_separator
+from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 # src.string.date_pieces.get
 # get_bag
@@ -9,19 +10,19 @@ def t_0():
   x = '19 Nov 2021'
   y = set(['19', 'Nov', '2021'])
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_1():
   x = '2021-11-19'
   y = set(['2021', '11', '19'])
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_2():
   x = '28/03/2022'
   y = set(['2022', '03', '28'])
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t():
   if not t_0(): return pf('t_0 failed')
