e3f4dcec2deb248f5335a15b641c6a4460321e88
John Forbes <john.robert.forbes@gmail.com>
2023-07-11 00:52:54 +1000

    Updated hak/one/dict/period/financial_year/get_end_date.py.

--- hak/one/dict/period/financial_year/get_end_date.py
+++ hak/one/dict/period/financial_year/get_end_date.py
@@ -4,10 +4,10 @@ from hak.one.dict.period.financial_year.make import f as mkfy
 from hak.pxyz import f as pxyz
 
 # get_ω_date
-f = lambda x: date(x['final_year'], 6, 30)
+f = lambda x: date(x['financial_year']['final_year'], 6, 30)
 
 def t():
-  x = mkfy({'start_year': 2022})
+  x = {'financial_year': mkfy({'start_year': 2022})}
   y = date(2023, 6, 30)
   z = f(x)
   return pxyz(x, y, z)
