54446c8d4c3683273370fa385375fa56c0abae96
John Forbes <john.robert.forbes@gmail.com>
2023-07-10 23:26:15 +1000

    Updated hak/one/string/year/is_a.py.

--- hak/one/string/year/is_a.py
+++ hak/one/string/year/is_a.py
@@ -1,4 +1,4 @@
-from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 # src.string.year.is_a
 # is_year
@@ -8,4 +8,4 @@ def t():
   x = '2022'
   y = True
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
