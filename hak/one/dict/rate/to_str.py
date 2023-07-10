785a3b75feeed5d53e0f98f427617bff3630e155
John Forbes <john.robert.forbes@gmail.com>
2023-07-10 23:21:08 +1000

    Updated hak/one/dict/rate/to_str.py.

--- hak/one/dict/rate/to_str.py
+++ hak/one/dict/rate/to_str.py
@@ -1,6 +1,6 @@
-from hak.one.string.print_and_return_false import f as pf
 from hak.one.dict.rate.make import f as make_rate
 from hak.one.dict.rate.to_float import f as to_float
+from hak.pxyz import f as pxyz
 
 # __str__
 f = lambda x: f"{to_float(x):.6f}"
@@ -9,4 +9,4 @@ def t():
   x = {'numerator': 710, 'denominator': 113}
   y = '6.283186'
   z = f(make_rate(x['numerator'], x['denominator']))
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
