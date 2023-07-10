bf0a3b3131ac07a6001ac658326a139b351f4661
John Forbes <john.robert.forbes@gmail.com>
2023-07-10 23:22:13 +1000

    Updated hak/one/dict/values/numbers/positive/count.py.

--- hak/one/dict/values/numbers/positive/count.py
+++ hak/one/dict/values/numbers/positive/count.py
@@ -1,5 +1,5 @@
-from hak.one.string.print_and_return_false import f as pf
 from hak.many.bools.count_true import f as count_true
+from hak.pxyz import f as pxyz
 
 # positives
 f = lambda x: count_true([_ > 0 for _ in [x[k] for k in x]]) if x else 0
@@ -8,4 +8,4 @@ def t():
   x = {'a': 1, 'b': 0, 'c': -1, 'd': 2, 'e': -2, 'f': -3}
   y = 2
   z = f(x)
-  return y == z or pf([f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
