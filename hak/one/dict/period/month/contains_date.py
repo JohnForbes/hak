c122f413956861b65f0dbe286bde60657daf0789
John Forbes <john.robert.forbes@gmail.com>
2023-07-11 00:54:24 +1000

    Updated hak/one/dict/period/month/contains_date.py.

--- hak/one/dict/period/month/contains_date.py
+++ hak/one/dict/period/month/contains_date.py
@@ -4,10 +4,10 @@ from hak.one.dict.period.month.get_end_date import f as get_ω_d
 from hak.one.string.print_and_return_false import f as pf
 
 # contains_date
-f = lambda x: get_α_d(x['month']) <= x['date'] <= get_ω_d(x['month'])
+f = lambda x: get_α_d(x) <= x['date'] <= get_ω_d(x)
 
 def t():
-  x = {'month': {'year': 2022, 'month_number': 1}, 'date': date(2022, 1, 10)}
+  x = {'month': {'year': 2022, 'number': 1}, 'date': date(2022, 1, 10)}
   y = True
   z = f(x)
   return y == z or pf(f'expected {x["date"]} to be in {x["month"]}')
