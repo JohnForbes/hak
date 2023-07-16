4a0b30c39bcf3720a2cd04d79da7efcbbe59231b
John Forbes <john.robert.forbes@gmail.com>
2023-07-16 11:22:16 +1000

    Updated hak/one/dict/table/get_field_widths.py.

--- hak/one/dict/table/get_field_widths.py
+++ hak/one/dict/table/get_field_widths.py
@@ -1,6 +1,7 @@
 from hak.one.string.print_and_return_false import f as pf
 from hak.one.dict.cell.get_width import f as get_w
-from hak.one.dict.quantity.make import f as make_quantity
+from hak.one.dict.rate.make import f as make_rate
+from hak.one.dict.rate.to_num import f as to_num
 
 f = lambda x: {
   k: max([
@@ -27,9 +28,9 @@ def t_b():
   x = {
     'field_names': list('abcde'),
     'records': [
-      {'a':  0, 'b':  1, 'c':  2, 'd': make_quantity( 3, 'm'), 'e':  4},
-      {'a':  5, 'b':  6, 'c':  7, 'd': make_quantity( 8, 'm'), 'e':  9},
-      {'a': 10, 'b': 11, 'c': 12, 'd': make_quantity(13, 'm'), 'e': 14},
+      {'a':  0, 'b':  1, 'c':  2, 'd': to_num(make_rate( 3, 1, 'm')), 'e':  4},
+      {'a':  5, 'b':  6, 'c':  7, 'd': to_num(make_rate( 8, 1, 'm')), 'e':  9},
+      {'a': 10, 'b': 11, 'c': 12, 'd': to_num(make_rate(13, 1, 'm')), 'e': 14},
     ]
   }
   y = {k: 2 for k in list('abcde')}
