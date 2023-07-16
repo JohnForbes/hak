5368ee91f1e3e979f6d9001c9671dfce21774aa9
John Forbes <john.robert.forbes@gmail.com>
2023-07-16 11:22:01 +1000

    Updated hak/one/dict/rate/to_float.py.

--- hak/one/dict/rate/to_float.py
+++ hak/one/dict/rate/to_float.py
@@ -1,20 +1,23 @@
 from hak.one.string.print_and_return_false import f as pf
+from hak.one.dict.rate.make import f as make_rate
 
 def f(x):
-  if not isinstance(x, dict): raise ValueError(f'{x} is not a dict')
-  if 'numerator' not in x: raise KeyError(f'{x} is missing numerator key')
-  if 'denominator' not in x: raise KeyError(f'{x} is missing denominator key')
-  return (
-    0 if (x == 0 or (x['numerator'] == 0 and x['denominator'] == 0)) else
-    x['numerator']/x['denominator']
-  )
+  if x['denominator'] == 0: raise ZeroDivisionError('denominator must not be 0')
+  return x['numerator']/x['denominator']
 
-def t_a():
-  x = {'numerator': 1, 'denominator': 2}
-  y = 0.5
-  z = f(x)
+def t_int_as_float():
+  x = make_rate(2, 1, '1')
+  y = type(2.0)
+  z = type(f(x))
+  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+
+def t_float():
+  x = make_rate(1, 2, '1')
+  y = type(0.5)
+  z = type(f(x))
   return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
 
 def t():
-  if not t_a(): return pf('!t_a')
+  if not t_int_as_float(): return pf('!t_int_as_float')
+  if not t_float(): return pf('!t_float')
   return True
