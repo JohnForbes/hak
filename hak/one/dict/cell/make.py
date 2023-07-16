eca307b53da1d00ca6437dc32eefd917cbef4f8c
John Forbes <john.robert.forbes@gmail.com>
2023-07-16 11:21:07 +1000

    Updated hak/one/dict/cell/make.py.

--- hak/one/dict/cell/make.py
+++ hak/one/dict/cell/make.py
@@ -2,22 +2,19 @@ from hak.one.dict.cell.to_str import f as to_str
 from hak.one.string.colour.decolour import f as decol
 from hak.one.string.print_and_return_false import f as pf
 from hak.pxyz import f as pxyz
-from hak.one.dict.quantity.make import f as make_quantity
+from hak.one.dict.rate.make import f as make_rate
 from datetime import date
-from hak.one.dict.get_or_default import f as get_or_default
-from hak.one.dict.quantity.is_a import f as is_quantity
+from hak.one.dict.rate.is_a import f as is_rate
+from hak.one.dict.rate.to_float import f as to_float
 
 # make_cell
 # src.cell.make
 def f(x):
-  val = x['value']['value'] if is_quantity(x['value']) else x['value']
+  val = to_float(x['value']) if is_rate(x['value']) else x['value']
   _val_str = to_str(val)
-  
   _width = x['width']
-  
   _ = _width - len(decol(f'{_val_str:>{_width}}'))
   left_pad = ' '*_
-
   return left_pad + f'{_val_str:>{_width}}'
 
 def t_0():
@@ -27,9 +24,12 @@ def t_0():
   z = f(x)
   return pxyz(x, y, z)
 
-def t_quantity():
-  x = {'value': make_quantity(1, 'm'), 'width': 1}
-  y = '1'
+def t_rate():
+  x = {
+    'value': make_rate(numerator=1, denominator=1, unit='m'),
+    'width': 1
+  }
+  y = '1.00'
   z = f(x)
   return pxyz(x, y, z)
 
@@ -46,23 +46,23 @@ def t_description():
   z = f(x)
   return pxyz(x, y, z)
 
-def t_USD_quantity():
-  x = {'value': {'value': 5472.0, 'unit': 'USD'}, 'width': 8}
+def t_USD_rate():
+  x = {'value': make_rate(5472, 1, 'USD'), 'width': 8}
   y = ' 5472.00'
   z = f(x)
   return pxyz(x, y, z)
 
 def t_asset_nabtrade_cash_AUD():
-  x = {'value': {'value': -7350.89, 'unit': 'AUD'}, 'width': 8}
+  x = {'value': make_rate(-7350.89, 1, 'AUD'), 'width': 8}
   y = '-7350.89'
   z = f(x)
   return pxyz(x, y, z)
 
 def t():
   if not t_0(): return pf('t_0 failed')
-  if not t_quantity(): return pf('t_quantity failed')
+  if not t_rate(): return pf('t_rate failed')
   if not t_date(): return pf('!t_date')
   if not t_description(): return pf('!t_description')
-  if not t_USD_quantity(): return pf('!t_USD_quantity')
+  if not t_USD_rate(): return pf('!t_USD_rate')
   if not t_asset_nabtrade_cash_AUD(): return pf('!t_asset_nabtrade_cash_AUD')
   return True
