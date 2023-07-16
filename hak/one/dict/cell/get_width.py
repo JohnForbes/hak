9e05a64c7509b2757694047bc4e629fe719bade7
John Forbes <john.robert.forbes@gmail.com>
2023-07-16 13:09:53 +1000

    Updated hak/one/dict/cell/get_width.py.

--- hak/one/dict/cell/get_width.py
+++ hak/one/dict/cell/get_width.py
@@ -5,18 +5,25 @@ from hak.one.string.print_and_return_false import f as pf
 from hak.one.dict.rate.make import f as make_rate
 from hak.one.dict.rate.is_a import f as is_rate
 from hak.one.dict.rate.to_float import f as to_float
+from hak.one.dict.rate.to_str_frac import f as to_str_frac
 
 def f(x):
   val = x['value']
   
   unit_width = len(val['unit']) if is_rate(val) else 0
+  header_word_widths = [len(i) for i in x['field_name'].split('_')]
+  
 
-  if is_rate(val):
-    val = to_float(val)
+  if x['field_name'].startswith('rate_'):
+    pass
+    value_str_width = len(decol(to_str_frac(val)))
+  else:
+    if is_rate(val): val = to_float(val)
+    value_str_width = len(decol(to_str(val)))
 
   return max([
-    *[len(i) for i in x['field_name'].split('_')],
-    len(decol(to_str(val))),
+    *header_word_widths,
+    value_str_width,
     unit_width
   ])
 
@@ -50,10 +57,20 @@ def t_quantity_long_unit():
   z = f(x)
   return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
 
+def t_k_starts_with_rate():
+  x = {
+    'value': make_rate(547200, 735089, 'USD/AUD'),
+    'field_name': 'rate_USD_per_AUD'
+  }
+  y = len('547200/735089')
+  z = f(x)
+  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
-  if not t_2(): return pf('t_2 failed')
-  if not t_quantity_short_unit(): return pf('t_quantity_short_unit failed')
-  if not t_quantity_long_unit(): return pf('t_quantity_long_unit failed')
+  if not t_0(): return pf('!t_0')
+  if not t_1(): return pf('!t_1')
+  if not t_2(): return pf('!t_2')
+  if not t_quantity_short_unit(): return pf('!t_quantity_short_unit')
+  if not t_quantity_long_unit(): return pf('!t_quantity_long_unit')
+  if not t_k_starts_with_rate(): return pf('!t_k_starts_with_rate')
   return True
