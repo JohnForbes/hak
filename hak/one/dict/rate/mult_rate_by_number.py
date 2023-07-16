d1efacae57d736b54cbbcaf5f84603b239d358e8
John Forbes <john.robert.forbes@gmail.com>
2023-07-16 11:21:52 +1000

    Updated hak/one/dict/rate/mult_rate_by_number.py.

--- hak/one/dict/rate/mult_rate_by_number.py
+++ hak/one/dict/rate/mult_rate_by_number.py
@@ -6,26 +6,26 @@ from hak.one.number.is_a import f as is_num
 def f(u, v):
   if not is_rate(u): raise ValueError(f'u: {u} is not a rate')
   if not is_num(v): raise ValueError(f'v: {v} is not a number')
-  return make_rate(u['numerator']*v, u['denominator'])
+  return make_rate(u['numerator']*v, u['denominator'], '1')
 
 def t_a():
-  u = {'numerator': 1, 'denominator': 3}
+  u = {'numerator': 1, 'denominator': 3, 'unit': '1'}
   v = 1
-  y = {'numerator': 1, 'denominator': 3}
+  y = {'numerator': 1, 'denominator': 3, 'unit': '1'}
   z = f(u, v)
   return y == z or pf([f"u: {u}", f"v: {v}", f"y: {y}", f"z: {z}"])
 
 def t_b():
-  u = {'numerator': 9, 'denominator': 7}
+  u = {'numerator': 9, 'denominator': 7, 'unit': '1'}
   v = 5
-  y = {'numerator': 45, 'denominator': 7}
+  y = {'numerator': 45, 'denominator': 7, 'unit': '1'}
   z = f(u, v)
   return y == z or pf([f"u: {u}", f"v: {v}", f"y: {y}", f"z: {z}"])
 
 def t_c():
-  u = {'numerator': 1, 'denominator': 3}
+  u = {'numerator': 1, 'denominator': 3, 'unit': '1'}
   v = 3
-  y = {'numerator': 1, 'denominator': 1}
+  y = {'numerator': 1, 'denominator': 1, 'unit': '1'}
   z = f(u, v)
   return y == z or pf([f"u: {u}", f"v: {v}", f"y: {y}", f"z: {z}"])
 
