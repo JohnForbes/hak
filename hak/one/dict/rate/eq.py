2b1346ebfbe5cf174f5e0423c020f59d61bd0dd7
John Forbes <john.robert.forbes@gmail.com>
2023-07-16 11:21:37 +1000

    Updated hak/one/dict/rate/eq.py.

--- hak/one/dict/rate/eq.py
+++ hak/one/dict/rate/eq.py
@@ -5,22 +5,22 @@ from hak.one.dict.rate.make import f as make_rate
 f = lambda u, v: make_rate(**u) == make_rate(**v)
 
 def t_true_a():
-  x_u = {'numerator': 1, 'denominator': 2}
-  x_v = {'numerator': 1.0, 'denominator': 2.0}
+  x_u = {'numerator': 1, 'denominator': 2, 'unit': '1'}
+  x_v = {'numerator': 1.0, 'denominator': 2.0, 'unit': '1'}
   y = True
   z = f(x_u, x_v)
   return y == z or pf([f"x_u: {x_u}", f"x_v: {x_v}", f"y: {y}", f"z: {z}"])
 
 def t_true_b():
-  x_u = {'numerator': 0.25, 'denominator': 0.5}
-  x_v = {'numerator': 10, 'denominator': 20}
+  x_u = {'numerator': 0.25, 'denominator': 0.5, 'unit': '1'}
+  x_v = {'numerator': 10, 'denominator': 20, 'unit': '1'}
   y = True
   z = f(x_u, x_v)
   return y == z or pf([f"x_u: {x_u}", f"x_v: {x_v}", f"y: {y}", f"z: {z}"])
 
 def t_false():
-  x_u = {'numerator': 1, 'denominator': 2}
-  x_v = {'numerator': 2, 'denominator': 3}
+  x_u = {'numerator': 1, 'denominator': 2, 'unit': '1'}
+  x_v = {'numerator': 2, 'denominator': 3, 'unit': '1'}
   y = False
   z = f(x_u, x_v)
   return y == z or pf([f"x_u: {x_u}", f"x_v: {x_v}", f"y: {y}", f"z: {z}"])
