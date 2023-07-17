824fdf754a87c31840b80fee69ecb24ed4bcbb37
John Forbes <john.robert.forbes@gmail.com>
2023-07-17 16:40:45 +1000

    Updated hak/one/dict/remove_zeroes.py.

--- hak/one/dict/remove_zeroes.py
+++ hak/one/dict/remove_zeroes.py
@@ -1,6 +1,6 @@
 from hak.one.dict.rate.is_a import f as is_rate
 from hak.one.dict.rate.make import f as make_rate
-from hak.one.dict.rate.to_float import f as calc_rate_value
+from hak.one.dict.rate.to_float import f as rate_to_float
 from hak.one.string.print_and_return_false import f as pf
 from hak.pxyz import f as pxyz
 
@@ -8,9 +8,7 @@ f = lambda x: {
   k: x[k]
   for k
   in x
-  if (
-    calc_rate_value(x[k]) if is_rate(x[k]) else x[k]
-  ) != 0
+  if (rate_to_float(x[k]) if is_rate(x[k]) else x[k]) != 0
 }
 
 def t_0():
@@ -20,13 +18,13 @@ def t_0():
   return pxyz(x, y, z)
 
 def t_1():
-  x = {'a': 0, 'b': 1, 'rate_a': make_rate(1, 2, '1')}
-  y = {'b': 1, 'rate_a': make_rate(1, 2, '1')}
+  x = {'a': 0, 'b': 1, 'rate_a': make_rate(1, 2, {})}
+  y = {'b': 1, 'rate_a': make_rate(1, 2, {})}
   z = f(x)
   return pxyz(x, y, z)
 
 def t_2():
-  x = {'a': 0, 'b': 1, 'rate_a': make_rate(0, 2, '1')}
+  x = {'a': 0, 'b': 1, 'rate_a': make_rate(0, 2, {})}
   y = {'b': 1}
   z = f(x)
   return pxyz(x, y, z)
