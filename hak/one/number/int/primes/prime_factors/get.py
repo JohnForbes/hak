4f50a1ca769e117c6e53bb7cf1bd4599fbc8553e
John Forbes <john.robert.forbes@gmail.com>
2023-07-17 16:41:00 +1000

    Updated hak/one/number/int/primes/prime_factors/get.py.

--- hak/one/number/int/primes/prime_factors/get.py
+++ hak/one/number/int/primes/prime_factors/get.py
@@ -7,6 +7,7 @@ from hak.one.file.get_next_line_as_int import f as get_next_prime
 from hak.one.file.zip.extract import f as extract
 from hak.one.number.int.primes.download_prime_10 import f as download_prime_10
 from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 def build_freq_dict(x, primes):
   queue = deque([1, *primes])
@@ -44,43 +45,43 @@ def t_2():
   x = 2
   y = {2: 1}
   z = f(x)
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t_3():
   x = 3
   y = {3: 1}
   z = f(x)
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t_4():
   x = 4
   y = {2: 2}
   z = f(x)
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t_10():
   x = 10
   y = {2: 1, 5: 1}
   z = f(x)
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t_360():
   x = 360
   y = {2: 3, 3: 2, 5: 1}
   z = f(x)
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t_7093094658085993():
   x = 7093094658085993
   y = {71: 1, 1109: 1, 33757: 1, 2668591: 1}
   z = f(x)
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t_735809():
   x = 7093094658085993
   y = {17929: 1, 41: 1}
   z = f(x)
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t():
   if not t_2(): return pf('!t_2')
