2d058f51e40dd324a82ebc14c3ea5d92e8e01c77
John Forbes <john.robert.forbes@gmail.com>
2023-07-17 16:40:30 +1000

    Updated hak/one/dict/rate/to_num.py.

--- hak/one/dict/rate/to_num.py
+++ hak/one/dict/rate/to_num.py
@@ -1,5 +1,6 @@
 from hak.one.string.print_and_return_false import f as pf
 from hak.one.dict.rate.make import f as make_rate
+from hak.pxyz import f as pxyz
 
 def f(x):
   if x['denominator'] == 0: raise ZeroDivisionError('denominator must not be 0')
@@ -7,16 +8,16 @@ def f(x):
   return x['numerator']/x['denominator']
 
 def t_int():
-  x = make_rate(2, 1, '1')
+  x = make_rate(2, 1, {'int': 1})
   y = type(2)
   z = type(f(x))
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t_float():
-  x = make_rate(1, 2, '1')
+  x = make_rate(1, 2, {'float': 1})
   y = type(0.5)
   z = type(f(x))
-  return y == z or pf([f"x: {x}", f"y: {y}", f"z: {z}"])
+  return pxyz(x, y, z)
 
 def t():
   if not t_int(): return pf('!t_int')
