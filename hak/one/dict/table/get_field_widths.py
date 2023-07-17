2b72cde6aa10d882999c9f96969cecfbd91daf82
John Forbes <john.robert.forbes@gmail.com>
2023-07-17 16:40:50 +1000

    Updated hak/one/dict/table/get_field_widths.py.

--- hak/one/dict/table/get_field_widths.py
+++ hak/one/dict/table/get_field_widths.py
@@ -1,7 +1,8 @@
-from hak.one.string.print_and_return_false import f as pf
 from hak.one.dict.cell.get_width import f as get_w
 from hak.one.dict.rate.make import f as make_rate
 from hak.one.dict.rate.to_num import f as to_num
+from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 f = lambda x: {
   k: max([
@@ -22,20 +23,38 @@ def t_a():
   }
   y = {k: 2 for k in list('abcde')}
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_b():
   x = {
     'field_names': list('abcde'),
     'records': [
-      {'a':  0, 'b':  1, 'c':  2, 'd': to_num(make_rate( 3, 1, 'm')), 'e':  4},
-      {'a':  5, 'b':  6, 'c':  7, 'd': to_num(make_rate( 8, 1, 'm')), 'e':  9},
-      {'a': 10, 'b': 11, 'c': 12, 'd': to_num(make_rate(13, 1, 'm')), 'e': 14},
+      {
+        'a':  0,
+        'b':  1,
+        'c':  2,
+        'd': to_num(make_rate( 3, 1, {'m': 1})),
+        'e':  4
+      },
+      {
+        'a':  5,
+        'b':  6,
+        'c':  7,
+        'd': to_num(make_rate( 8, 1, {'m': 1})),
+        'e':  9
+      },
+      {
+        'a': 10,
+        'b': 11,
+        'c': 12,
+        'd': to_num(make_rate(13, 1, {'m': 1})),
+        'e': 14
+      },
     ]
   }
   y = {k: 2 for k in list('abcde')}
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t():
   if not t_a(): return pf('!t_a')
