b97c2631f0018867c25822527d64c796b81bea4d
John Forbes <john.robert.forbes@gmail.com>
2023-07-17 16:39:56 +1000

    Updated hak/one/dict/rate/eq.py.

--- hak/one/dict/rate/eq.py
+++ hak/one/dict/rate/eq.py
@@ -5,29 +5,29 @@ from hak.one.dict.rate.make import f as make_rate
 f = lambda u, v: make_rate(**u) == make_rate(**v)
 
 def t_true_a():
-  x_u = {'numerator': 1, 'denominator': 2, 'unit': '1'}
-  x_v = {'numerator': 1.0, 'denominator': 2.0, 'unit': '1'}
+  x_u = make_rate(  1,   2, {'1': 0})
+  x_v = make_rate(1.0, 2.0, {'1': 0})
   y = True
   z = f(x_u, x_v)
   return y == z or pf([f"x_u: {x_u}", f"x_v: {x_v}", f"y: {y}", f"z: {z}"])
 
 def t_true_b():
-  x_u = {'numerator':  0.25, 'denominator': 0.5, 'unit': '1'}
-  x_v = {'numerator': 10, 'denominator': 20, 'unit': '1'}
+  x_u = make_rate( 0.25, 0.5, {'1': 0})
+  x_v = make_rate(10, 20, {'1': 0})
   y = True
   z = f(x_u, x_v)
   return y == z or pf([f"x_u: {x_u}", f"x_v: {x_v}", f"y: {y}", f"z: {z}"])
 
 def t_false():
-  x_u = {'numerator': 1, 'denominator': 2, 'unit': '1'}
-  x_v = {'numerator': 2, 'denominator': 3, 'unit': '1'}
+  x_u = make_rate(1, 2, {'1': 0})
+  x_v = make_rate(2, 3, {'1': 0})
   y = False
   z = f(x_u, x_v)
   return y == z or pf([f"x_u: {x_u}", f"x_v: {x_v}", f"y: {y}", f"z: {z}"])
 
 def t_false_different_units():
-  x_u = {'numerator': 1, 'denominator': 2, 'unit': 'a'}
-  x_v = {'numerator': 2, 'denominator': 3, 'unit': 'b'}
+  x_u = make_rate(1, 2, {'a': 1})
+  x_v = make_rate(2, 3, {'b': 1})
   y = False
   z = f(x_u, x_v)
   return y == z or pf([f"x_u: {x_u}", f"x_v: {x_v}", f"y: {y}", f"z: {z}"])
