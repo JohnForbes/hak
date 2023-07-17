3a930b39e204bb193e95668ffc670a200b286257
John Forbes <john.robert.forbes@gmail.com>
2023-07-17 16:39:08 +1000

    Updated hak/one/dict/cell/get_width.py.

--- hak/one/dict/cell/get_width.py
+++ hak/one/dict/cell/get_width.py
@@ -6,19 +6,20 @@ from hak.one.dict.rate.make import f as make_rate
 from hak.one.dict.rate.is_a import f as is_rate
 from hak.one.dict.rate.to_float import f as to_float
 from hak.one.dict.rate.to_str_frac import f as to_str_frac
+from hak.pxyz import f as pxyz
 
 def f(x):
   val = x['value']
   
-  unit_width = len(val['unit']) if is_rate(val) else 0
+  unit_width = len(str(val['unit'])) if is_rate(val) else 0
   header_word_widths = [len(i) for i in x['field_name'].split('_')]
 
   if x['field_name'].startswith('rate_'):
     val_str = to_str_frac(val) if is_rate(val) else ''
-    value_str_width = len(decol(val_str))
   else:
-    if is_rate(val): val = to_float(val)
-    value_str_width = len(decol(to_str(val)))
+    val_str = to_str(to_float(val) if is_rate(val) else val)
+  
+  value_str_width = len(decol(val_str))
 
   return max([*header_word_widths, value_str_width, unit_width])
 
@@ -26,40 +27,47 @@ def t_0():
   x = {'value': False, 'field_name': 'a'}
   y = 1
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_1():
   x = {'value': 'a', 'field_name': 'aa'}
   y = 2
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_2():
   x = {'value': red('-'), 'field_name': 'is_revenue'}
   y = len('revenue')
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_quantity_short_unit():
-  x = {'value': make_rate(12.34, 1, 'm'), 'field_name': 'length'}
-  y = len('length')
+  x = {'value': make_rate(12.34, 1, {'m': 1}), 'field_name': 'length'}
+  y = len(str({'m': 1}))
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_quantity_long_unit():
-  x = {'value': make_rate(12.34, 1, 'lightyear'), 'field_name': 'length'}
-  y = len('lightyear')
+  x = {
+    'value': make_rate(
+      12.34,
+      1,
+      {'lightyear': 1}
+    ),
+    'field_name': 'length'
+  }
+  y = len(str({'lightyear': 1}))
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_k_starts_with_rate():
   x = {
-    'value': make_rate(547200, 735089, 'USD/AUD'),
+    'value': make_rate(547200, 735089, {'USD': 1, 'AUD': -1}),
     'field_name': 'rate_USD_per_AUD'
   }
-  y = len('547200/735089')
+  y = len(str(x['value']['unit']))
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t():
   if not t_0(): return pf('!t_0')
