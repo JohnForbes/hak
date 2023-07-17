583ecd5f553b9605759f4d29933fda3da7b89f22
John Forbes <john.robert.forbes@gmail.com>
2023-07-17 16:38:48 +1000

    Updated hak/many/dicts/get_all_keys.py.

--- hak/many/dicts/get_all_keys.py
+++ hak/many/dicts/get_all_keys.py
@@ -1,14 +1,14 @@
-from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 f = lambda x: sorted(list(set([k for d in x for k in d.keys()])))
 
 def t():
   x = [
-    {'a': True, 'b': True},
-    {'a': True, 'b': False},
+    {'a':  True, 'b':  True},
+    {'a':  True, 'b': False},
     {'a': False, 'b': False},
-    {'a': False, 'b': True, 'c': None}
+    {'a': False, 'b':  True, 'c': None}
   ]
   y = ['a', 'b', 'c']
   z = f(x)
-  return y == z or pf([f"x: {x}", f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
