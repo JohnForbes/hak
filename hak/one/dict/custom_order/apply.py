fc238b27206f4bfda8e92ec41a44f80de04ba07c
John Forbes <john.robert.forbes@gmail.com>
2023-07-20 09:29:42 +1000

    Updated hak/one/dict/custom_order/apply.py.

--- hak/one/dict/custom_order/apply.py
+++ hak/one/dict/custom_order/apply.py
@@ -3,12 +3,12 @@ from hak.pxyz import f as pxyz
 
 # apply_custom_order
 def f(x):
-  field_order = deepcopy(x['field_order'])
-  field_names = [n for n in x['field_names'] if n not in field_order]
-  return field_order + field_names
+  order = deepcopy(x['order'])
+  names = [n for n in x['names'] if n not in order]
+  return order + names
 
 def t():
-  x = {'field_order': list('cba'), 'field_names': list('abcdef')}
+  x = {'order': list('cba'), 'names': list('abcdef')}
   y = ['c', 'b', 'a', 'd', 'e', 'f']
   z = f(x)
   return pxyz(x, y, z)
