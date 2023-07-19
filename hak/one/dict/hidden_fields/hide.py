4489ea4a44249fa48557639dcdf5df1ab57c2906
John Forbes <john.robert.forbes@gmail.com>
2023-07-20 09:29:50 +1000

    Updated hak/one/dict/hidden_fields/hide.py.

--- hak/one/dict/hidden_fields/hide.py
+++ hak/one/dict/hidden_fields/hide.py
@@ -1,9 +1,9 @@
 from hak.pxyz import f as pxyz
 
-f = lambda x: [k for k in x['field_names'] if k not in set(x['hidden_fields'])]
+f = lambda x: [k for k in x['names'] if k not in set(x['hidden'])]
 
 def t():
-  x = {'hidden_fields': list('ace'), 'field_names': list('abcde')}
+  x = {'hidden': list('ace'), 'names': list('abcde')}
   y = list('bd')
   z = f(x)
   return pxyz(x, y, z)
