128f97bb8e7bfa42ed8c8c2f6b25465ef2361296
John Forbes <john.robert.forbes@gmail.com>
2023-08-01 17:13:15 +1000

    Updated hak/one/dict/to_max_line_width_str.py.

--- hak/one/dict/to_max_line_width_str.py
+++ hak/one/dict/to_max_line_width_str.py
@@ -1,18 +1,15 @@
 # ignore_overlength_lines
 from hak.one.string.print_and_return_false import f as pf
-from hak.one.string.colour.bright.cyan import f as cy
-from hak.one.string.colour.bright.blue import f as bl
-from hak.one.string.colour.bright.magenta import f as mg
 from hak.many.strings.get_last_line_width import f as get_last_line_width
-from hak.insert_new_line_at_best_location import f as insert_new_line_at_best_location
+from hak.dict_string_to_dict_string_limited_width import f as dict_string_to_dict_string_limited_width
+from hak.pxyz import f as pxyz
 
 def f(x, w=80):
-  _x = str(x)
-  if len(_x) <= w: return _x
+  if len(str(x)) <= w: return str(x)
 
   result = ', '.join([f"'{k}': {x[k]}" for k in x])
   while get_last_line_width(result) > w-2:
-    result = insert_new_line_at_best_location(result)
+    result = dict_string_to_dict_string_limited_width(result)
 
   return '{\n  '+result+'\n}'
 
@@ -20,18 +17,13 @@ def t_short():
   x = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8}
   y = "{'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8}"
   z = f(x)
-  return y == z or pf(['y != z', f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
 
 def t_w():
   x = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9}
   y = "{'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9}"
   z = f(x)
-  return y == z or pf([
-    'y != z',
-    f'x:   {cy(x)}',
-    f'[y]: {bl([y])}',
-    f'[z]: {mg([z])}'
-  ])
+  return pxyz(x, y, z)
 
 def t_too_long_a():
   x = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 10}
@@ -42,12 +34,7 @@ def t_too_long_a():
     "}",
   ])
   z = f(x)
-  return y == z or pf([
-    'y != z',
-    f'x: {cy(x)}',
-    f'[y]: {bl([y])}',
-    f'[z]: {mg([z])}'
-  ])
+  return pxyz(x, y, z)
 
 def t_too_long_b():
   x = {
@@ -61,12 +48,7 @@ def t_too_long_b():
     "}",
   ])
   z = f(x)
-  return y == z or pf([
-    'y != z',
-    f'x: {cy(x)}',
-    f'[y]: {bl([y])}',
-    f'[z]: {mg([z])}'
-  ])
+  return pxyz(x, y, z)
 
 def t_too_long_c():
   x = {chr(k+97): k for k in range(17)}
@@ -77,12 +59,7 @@ def t_too_long_c():
     "}",
   ])
   z = f(x)
-  return y == z or pf([
-    'y != z',
-    f'x: {cy(x)}',
-    f'[y]: {bl([y])}',
-    f'[z]: {mg([z])}'
-  ])
+  return pxyz(x, y, z)
 
 def t_too_long_d():
   x = {chr(k+97): k for k in range(18)}
@@ -94,12 +71,7 @@ def t_too_long_d():
     "}",
   ])
   z = f(x)
-  return y == z or pf([
-    'y != z',
-    f'x: {cy(x)}',
-    f'[y]: {bl([y])}',
-    f'[z]: {mg([z])}'
-  ])
+  return pxyz(x, y, z)
 
 def t_too_long_e():
   x = {chr(k+97): k for k in range(26)}
@@ -112,12 +84,7 @@ def t_too_long_e():
     "}",
   ])
   z = f(x)
-  return y == z or pf([
-    'y != z',
-    f'x: {cy(x)}',
-    f'[y]: {bl([y])}',
-    f'[z]: {mg([z])}'
-  ])
+  return pxyz(x, y, z)
 
 def t():
   if not t_short(): return pf('t_short() failed')
