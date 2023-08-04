99b3a48bf641a1477222ce71094c76a516a4cda9
John Forbes <john.robert.forbes@gmail.com>
2023-08-04 12:27:34 +1000

    Updated hak/many/dicts/records/k_branch/to_sorted_leaf_keys.py.

--- hak/many/dicts/records/k_branch/to_sorted_leaf_keys.py
+++ hak/many/dicts/records/k_branch/to_sorted_leaf_keys.py
@@ -1,10 +1,15 @@
+# ignore_overlength_lines
 from hak.pxyz import f as pxyz
 from hak.one.dict.get_sorted_keys import f as get_sorted_keys
 from hak.one.string.print_and_return_false import f as pf
 from hak.one.dict.rate.make import f as make_rate
+from datetime import date
 
 # records_k_branch_to_sorted_leaf_keys
-f = lambda records, field_name: get_sorted_keys(records[0][field_name])
+# f = lambda records, field_name: get_sorted_keys(records[0][field_name])
+def f(records, field_name):
+  y = get_sorted_keys(records[0][field_name])
+  return y
 
 def t_prices():
   x = {
@@ -74,8 +79,21 @@ def t_zloops():
   z = f(x, a)
   return pxyz(x, y, z)
 
+def t_date():
+  x = {
+    'records': [
+      {'date': date(2023, 7, 27), 'prices': {'apples': {'numerator': 1, 'denominator': 4, 'unit': {'$': 1, 'apple': -1}}, 'bananas': {'numerator': 1, 'denominator': 2, 'unit': {'$': 1, 'banana': -1}}}, 'volumes': {'applezzz': {'numerator': 1, 'denominator': 1, 'unit': {'apple': 1}}, 'bananazzz': {'numerator': 2, 'denominator': 1, 'unit': {'banana': 1}}, 'pearzzzzzz': {'numerator': 3, 'denominator': 1, 'unit': {'pear': 1}}}, 'zloops': {'zloop': {'numerator': 7, 'denominator': 1, 'unit': {'zloop': 1}}}}, 
+      {'date': date(2023, 7, 28), 'prices': {'apples': {'numerator': 3, 'denominator': 4, 'unit': {'$': 1, 'apple': -1}}, 'bananas': {'numerator': 1, 'denominator': 1, 'unit': {'$': 1, 'banana': -1}}}, 'volumes': {'applezzz': {'numerator': 4, 'denominator': 1, 'unit': {'apple': 1}}, 'bananazzz': {'numerator': 5, 'denominator': 1, 'unit': {'banana': 1}}, 'pearzzzzzz': {'numerator': 6, 'denominator': 1, 'unit': {'pear': 1}}}, 'zloops': {'zloop': {'numerator': 7, 'denominator': 1, 'unit': {'zloop': 1}}}}
+    ],
+    'field_name': 'date'
+  }
+  y = ['zloop']
+  z = f(**x)
+  return pxyz(x, y, z)
+
 def t():
   if not t_prices(): return pf('!t_prices')
   if not t_volumes(): return pf('!t_volumes')
   if not t_zloops(): return pf('!t_zloops')
+  # if not t_date(): return pf('!t_date')
   return True
