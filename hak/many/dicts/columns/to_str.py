30237985d31050acd0eb9222ca74178dd4630e37
John Forbes <john.robert.forbes@gmail.com>
2023-08-06 14:39:13 +1000

    Updated hak/many/dicts/columns/to_str.py.

--- hak/many/dicts/columns/to_str.py
+++ hak/many/dicts/columns/to_str.py
@@ -1,7 +1,8 @@
-from hak.one.string.print_and_return_false import f as pf
-from hak.pxyz import f as pxyz
+from datetime import date
 from hak.one.dict.column.make import f as make_column
 from hak.one.dict.column.to_str import f as column_to_str
+from hak.one.string.print_and_return_false import f as pf
+from hak.pxyz import f as pxyz
 
 def f(columns, separator='|'):
   column_strings = [column_to_str(c) for c in columns]
@@ -58,7 +59,40 @@ def t_1():
   z = f(**x)
   return pxyz(x, '\n'+y, '\n'+z)
 
+def t_date():
+  x = {
+    'columns': [
+      make_column('abc', [0, 1, 2, 3]),
+      make_column('ghi', [0, 10, 200, 3000]),
+      make_column('jklm', ['abc', 'blergh', 'wragh', 'hmmm']),
+      make_column(
+        'date',
+        [
+          date(2023, 1, 1),
+          date(2023, 2, 1),
+          date(2023, 3, 1),
+          date(2023, 4, 1)
+        ]
+      )
+
+    ],
+    'separator': '|'
+  }
+  y = '\n'.join([
+    '-----|------|--------|------------',
+    ' abc |  ghi |   jklm |       date ',
+    '-----|------|--------|------------',
+    '   0 |    0 |    abc | 2023-01-01 ',
+    '   1 |   10 | blergh | 2023-02-01 ',
+    '   2 |  200 |  wragh | 2023-03-01 ',
+    '   3 | 3000 |   hmmm | 2023-04-01 ',
+    '-----|------|--------|------------',
+  ])
+  z = f(**x)
+  return pxyz(x, '\n'+y, '\n'+z)
+
 def t():
   if not t_0(): return pf('!t_0')
   if not t_1(): return pf('!t_1')
+  if not t_date(): return pf('!t_date')
   return True
