9cb942b8656e0b90a44d8069e5a0cbd61751ea66
John Forbes <john.robert.forbes@gmail.com>
2023-08-07 11:22:43 +1000

    Added hak/one/string/find_last_char.py.

---
+++ hak/one/string/find_last_char.py
@@ -0,0 +1,9 @@
+from hak.one.string.print_and_return_false import f as pf
+
+f = lambda string, char: len(string)-string[::-1].find(char)-1
+
+def t():
+  x = {'string': 'a,b,c,de', 'char': ','}
+  y = 5
+  z = f(**x)
+  return y == z or pf(['y != z', f'x: {x}', f'y: {y}', f'z: {z}'])
