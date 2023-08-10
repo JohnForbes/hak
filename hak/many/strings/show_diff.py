79db4e2080ce95c36da3bf0605575fb762a8290a
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 23:09:18 +1000

    Updated hak/many/strings/show_diff.py.

--- hak/many/strings/show_diff.py
+++ hak/many/strings/show_diff.py
@@ -2,7 +2,7 @@ from hak.one.string.colour.bright.cyan import f as cy
 from hak.one.string.colour.bright.magenta import f as mg
 from hak.one.string.colour.bright.yellow import f as yl
 from hak.many.strings.find_first_diff import f as find_first_diff
-from hak.pf import f as pf
+from hak.puvyz import f as puvyz
 
 # show_diff
 
@@ -17,12 +17,12 @@ def f(u, v, w=64):
   ]
 
 def t():
-  x_u = 'abcdef'
-  x_v = 'abxdef'
+  u = 'abcdef'
+  v = 'abxdef'
   y = [
     '2',
     '\x1b[1;36mab\x1b[0;0m\x1b[1;33mc\x1b[0;0m\x1b[1;36mdef\x1b[0;0m',
     '\x1b[1;35mab\x1b[0;0m\x1b[1;33mx\x1b[0;0m\x1b[1;35mdef\x1b[0;0m'
   ]
-  z = f(x_u, x_v)
-  return y == z or pf(['y != z', f'y: {y}', f'z: {z}'])
+  z = f(u, v)
+  return puvyz(u, v, y, z)
