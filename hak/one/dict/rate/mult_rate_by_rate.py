45f549422d48f117fd1f71ab00d9b3665a45b4eb
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 23:10:17 +1000

    Updated hak/one/dict/rate/mult_rate_by_rate.py.

--- hak/one/dict/rate/mult_rate_by_rate.py
+++ hak/one/dict/rate/mult_rate_by_rate.py
@@ -1,6 +1,7 @@
 from hak.pf import f as pf
 from hak.one.dict.rate.make import f as make_rate
 from hak.one.dict.rate.is_a import f as is_rate
+from hak.puvyz import f as puvyz
 
 def f(u, v):
   if not is_rate(u): raise ValueError(f'u: {u} is not a rate')
@@ -22,21 +23,21 @@ def t_a():
   v = make_rate(3, 1, {'m': 1})
   y = make_rate(1, 1, {'m': 2})
   z = f(u, v)
-  return y == z or pf([f"u: {u}", f"v: {v}", f"y: {y}", f"z: {z}"])
+  return puvyz(u, v, y, z)
 
 def t_b():
   u = make_rate( 2,  3, {'s': 1})
   v = make_rate( 5,  7, {'s': 1})
   y = make_rate(10, 21, {'s': 2})
   z = f(u, v)
-  return y == z or pf([f"u: {u}", f"v: {v}", f"y: {y}", f"z: {z}"])
+  return puvyz(u, v, y, z)
 
 def t_c():
   u = make_rate( 13,  11, {})
   v = make_rate( 19,  17, {})
   y = make_rate(247, 187, {})
   z = f(u, v)
-  return y == z or pf([f"u: {u}", f"v: {v}", f"y: {y}", f"z: {z}"])
+  return puvyz(u, v, y, z)
 
 def t():
   if not t_a(): return pf('!t_a')
