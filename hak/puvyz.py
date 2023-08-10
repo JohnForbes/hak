f4303b57aaeb997b8ffd7f6538de476902332974
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 23:10:42 +1000

    Updated hak/puvyz.py.

--- hak/puvyz.py
+++ hak/puvyz.py
@@ -1,6 +1,5 @@
 from hak.pf import f as pf
 
-def f(u, v, y, z):
-  return y == z or pf([f'u: {u}', f'v: {v}', f'y: {y}', f'z: {z}'])
+f = lambda u, v, y, z: y==z or pf([f'u: {u}', f'v: {v}', f'y: {y}', f'z: {z}'])
 
 t = lambda: True
