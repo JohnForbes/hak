b37ae70debf72ecc6635f08721d34b0cdab40a76
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:26:30 +1000

    Updated hak/dict_string_to_limited_width_dict_string.py.

--- hak/dict_string_to_limited_width_dict_string.py
+++ hak/dict_string_to_limited_width_dict_string.py
@@ -1,6 +1,6 @@
 # ignore_overlength_lines
 from hak.pxyz import f as pxyz
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 
 # dict_string_to_limited_width_dict_string
 def f(x, w=80):
