504b8ff8ea5fdfb55ed180fb1c39326efdf5b45f
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:46:15 +1000

    Updated hak/one/dict/column/get_width.py.

--- hak/one/dict/column/get_width.py
+++ hak/one/dict/column/get_width.py
@@ -1,5 +1,5 @@
 from hak.one.dict.column.make import f as make_column
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 from hak.pxyz import f as pxyz
 
 f = lambda x: max(len(x['heading']), *[len(str(v)) for v in x['values']])
