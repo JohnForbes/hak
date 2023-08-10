3eb1c45219c5c9dcb4f26a26e373f3e8668fb0b1
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:48:12 +1000

    Updated hak/one/dict/period/month/contains_date.py.

--- hak/one/dict/period/month/contains_date.py
+++ hak/one/dict/period/month/contains_date.py
@@ -1,7 +1,7 @@
 from datetime import date
 from hak.one.dict.period.month.get_start_date import f as get_α_d
 from hak.one.dict.period.month.get_end_date import f as get_ω_d
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 
 # contains_date
 f = lambda x: get_α_d(x) <= x['date'] <= get_ω_d(x)
