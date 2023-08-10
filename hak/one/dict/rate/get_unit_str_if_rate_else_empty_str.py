da7a58d29b5ff747b6e766b13e7842d8f23b3cbb
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:48:51 +1000

    Updated hak/one/dict/rate/get_unit_str_if_rate_else_empty_str.py.

--- hak/one/dict/rate/get_unit_str_if_rate_else_empty_str.py
+++ hak/one/dict/rate/get_unit_str_if_rate_else_empty_str.py
@@ -1,6 +1,6 @@
 from hak.one.dict.get_or_default import f as get_or_default
 from hak.one.dict.rate.is_a import f as is_rate
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 from hak.one.dict.unit.to_str import f as to_str
 from hak.pxyz import f as pxyz
 
