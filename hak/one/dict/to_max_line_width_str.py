e3e0602ababdff2f1d247ae8c135ca7c477ee946
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:49:40 +1000

    Updated hak/one/dict/to_max_line_width_str.py.

--- hak/one/dict/to_max_line_width_str.py
+++ hak/one/dict/to_max_line_width_str.py
@@ -1,5 +1,5 @@
 # ignore_overlength_lines
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 from hak.many.strings.get_last_line_width import f as get_last_line_width
 from hak.dict_string_to_limited_width_dict_string import f as dict_string_to_limited_width_dict_string
 from hak.pxyz import f as pxyz
