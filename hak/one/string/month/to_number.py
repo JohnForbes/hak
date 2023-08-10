0a63cb8dd239af67e55e201140daca6b375bf36d
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:51:57 +1000

    Updated hak/one/string/month/to_number.py.

--- hak/one/string/month/to_number.py
+++ hak/one/string/month/to_number.py
@@ -1,5 +1,5 @@
 from hak.data.months import months
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 from hak.pxyz import f as pxyz
 
 # src.string.month.to_number
