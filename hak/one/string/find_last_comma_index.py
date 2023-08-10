7f3e09598eec0424e550b2920f531d24d0ace734
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 22:12:57 +1000

    Updated hak/one/string/find_last_comma_index.py.

--- hak/one/string/find_last_comma_index.py
+++ hak/one/string/find_last_comma_index.py
@@ -1,5 +1,5 @@
-from hak.pf import f as pf
 from hak.one.string.find_last_char import f as find_last_char
+from hak.pxyz import f as pxyz
 
 f = lambda x: find_last_char(x, ',')
 
@@ -7,4 +7,4 @@ def t():
   x = 'a,b,c,de'
   y = 5
   z = f(x)
-  return y == z or pf(['y != z', f'x: {x}', f'y: {y}', f'z: {z}'])
+  return pxyz(x, y, z)
