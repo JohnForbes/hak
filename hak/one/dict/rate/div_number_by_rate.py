d5a5fce4acc9d7ba081137db830fa08f0243070d
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:48:36 +1000

    Updated hak/one/dict/rate/div_number_by_rate.py.

--- hak/one/dict/rate/div_number_by_rate.py
+++ hak/one/dict/rate/div_number_by_rate.py
@@ -1,4 +1,4 @@
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 from hak.one.dict.rate.make import f as make_rate
 from hak.one.dict.rate.div_rate_by_rate import f as div_rate_by_rate
 
