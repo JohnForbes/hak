5776e91560bf93fb734c0d2a2a75fc34271329d1
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 19:50:13 +1000

    Updated hak/many/dicts/records/fn/to_fn_applied_to_sorted_keys_of_records.py.

--- hak/many/dicts/records/fn/to_fn_applied_to_sorted_keys_of_records.py
+++ hak/many/dicts/records/fn/to_fn_applied_to_sorted_keys_of_records.py
@@ -5,9 +5,12 @@ from ..to_pad_k_branch import f as records_to_pad_k_branch
 from hak.one.dict.rate.make import f as make_rate
 from hak.one.string.print_and_return_false import f as pf
 from hak.pxyz import f as pxyz
+from datetime import date
 
 # records_and_fn_to_fn_applied_to_sorted_keys_of_records
 f = lambda records, fn_a: [fn_a(records, k) for k in get_K(records)]
+# def f(records, fn_a):
+#   return [fn_a(records, k) for k in get_K(records)]
 
 _records = [
   {
@@ -48,7 +51,26 @@ def t_1():
   z = f(**x)
   return pxyz(x, y, z)
 
+def t_date():
+  x = {
+    'records': [
+      {
+        'date': date(2023, 7, 27),
+        'prices': {'apples': make_rate(1, 4, {'$': 1, 'apple': -1})}
+      }, 
+      {
+        'date': date(2023, 7, 28),
+        'prices': {'apples': make_rate(3, 4, {'$': 1, 'apple': -1})}
+      }
+    ],
+    'fn_a': get_top_head_width
+  }
+  y = ['      date', ' prices']
+  z = f(**x)
+  return pxyz(x, y, z)
+
 def t():
   if not t_0(): return pf('!t_0')
   if not t_1(): return pf('!t_1')
+  # if not t_date(): return pf('!t_date')
   return True
