a51142f953bd69c49b3d9cddbcde76c31136109e
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:44:29 +1000

    Updated hak/many/directories/get.py.

--- hak/many/directories/get.py
+++ hak/many/directories/get.py
@@ -3,7 +3,7 @@ from os.path import isdir
 from hak.one.directory.filepaths.get import f as get_filepaths
 from hak.one.directory.make import f as mkd
 from hak.one.directory.remove import f as rmd
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 
 f = lambda root: set([p for p in get_filepaths(root=root) if isdir(p)])
 
