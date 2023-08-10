9bd872b14bb9001728c07530186219565a07489e
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:49:59 +1000

    Updated hak/one/directory/filepaths/packages/get.py.

--- hak/one/directory/filepaths/packages/get.py
+++ hak/one/directory/filepaths/packages/get.py
@@ -3,7 +3,7 @@ from os.path import isfile
 from os import remove
 
 from hak.one.directory.filepaths.get import f as get_filepaths
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 from hak.one.directory.make import f as mkdir
 from hak.one.directory.remove import f as remove_dir
 from hak.one.file.save import f as save
