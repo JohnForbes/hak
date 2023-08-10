c5e5b86b567eea5e3e6a49785c46d496b3add0bc
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 21:45:27 +1000

    Updated hak/many/values/get_datatype.py.

--- hak/many/values/get_datatype.py
+++ hak/many/values/get_datatype.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from hak.one.dict.rate.make import f as make_rate
-from hak.one.string.print_and_return_false import f as pf
+from hak.pf import f as pf
 from hak.pxyz import f as pxyz
 from hak.one.dict.rate.is_a import f as is_a_rate
 from hak.one.dict.rate.make import f as make_rate
