a9fbdc2d4b60ecb1f82c8a51b8a43030cff0337a
John Forbes <john.robert.forbes@gmail.com>
2023-08-10 22:12:27 +1000

    Updated hak/one/dict/period/financial_year/gt.py.

--- hak/one/dict/period/financial_year/gt.py
+++ hak/one/dict/period/financial_year/gt.py
@@ -1,4 +1,4 @@
-from hak.pf import f as pf
+from hak.puvyz import f as puvyz
 from hak.one.dict.period.financial_year.make import f as mkfy
 
 # gt
@@ -9,4 +9,4 @@ def t():
   v = mkfy({'final_year': 2023})
   y = True
   z = f(u, v)
-  return y == z or pf([f'u: {u}', f'v: {v}', f'y: {y}', f'z: {z}'])
+  return puvyz(u, v, y, z)
