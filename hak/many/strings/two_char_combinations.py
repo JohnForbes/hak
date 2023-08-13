4815f3241e8234f1ed0f4baf2fa7234759f93ef3
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 00:21:56 +1000

    Updated hak/many/strings/two_char_combinations.py.

--- hak/many/strings/two_char_combinations.py
+++ hak/many/strings/two_char_combinations.py
@@ -1,8 +1,12 @@
 from itertools import combinations_with_replacement as cwr
 from string import ascii_letters, digits, punctuation
-_ch = ascii_letters + digits + punctuation + ' '
+_charset = ascii_letters + digits + punctuation + ' '
 
-f = lambda ch=_ch: [''.join(c) for n in range(2, 0, -1) for c in cwr(ch, n)]
+f = lambda charset=_charset: [
+  ''.join(c)
+  for n in range(2, 0, -1)
+  for c in cwr(charset, n)
+]
 
 def t():
   z = f()
