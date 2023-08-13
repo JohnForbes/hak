3755bcda831a307123dce2bb7f80bc0bb0a72efb
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 00:27:37 +1000

    Updated hak/one/string/line/last/length/get.py.

--- hak/one/string/line/last/length/get.py
+++ hak/one/string/line/last/length/get.py
@@ -1,7 +1,4 @@
-from hak.pf import f as pf
-from hak.one.string.colour.bright.cyan import f as cy
-from hak.one.string.colour.bright.blue import f as bl
-from hak.one.string.colour.bright.magenta import f as mg
+from hak.pxyz import f as pxyz
 
 f = lambda x: len(x.split('\n')[-1])
 
@@ -9,9 +6,4 @@ def t():
   x = "abc\ndefg\nhijklm"
   y = 6
   z = f(x)
-  return y == z or pf([
-    'y != z',
-    f'x: {cy(x)}',
-    f'[y]: {bl([y])}',
-    f'[z]: {mg([z])}'
-  ])
+  return pxyz([x], [y], [z])
