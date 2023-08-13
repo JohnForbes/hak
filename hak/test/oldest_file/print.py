603277cf1edfe64e7983cd82b370df8b9b198d51
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 00:29:38 +1000

    Updated hak/test/oldest_file/print.py.

--- hak/test/oldest_file/print.py
+++ hak/test/oldest_file/print.py
@@ -2,7 +2,7 @@ from os import remove
 from os.path import exists
 from os.path import getmtime
 
-from hak.one.dict.durations.to_tuple_list_sorted_by_duration import f as srt
+from hak.one.dict.durations.to_tuple_list_sorted_by_duration import f as sort
 from hak.one.file.pickle.load_if_exists import f as load_pickle
 from hak.one.file.pickle.save import f as save_pickle
 from hak.one.file.remove import f as remove
@@ -102,6 +102,26 @@ def make_Pi_t(python_filepaths, test_all, prev, last_mods): return (
 
 # print_oldest_file
 
+# from hak.many.strings.filepaths.py.testables.get import f as list_testables
+# from os.path import getmtime
+# from hak.one.file.pickle.load_if_exists import f as load_pickle
+# from hak.one.file.remove import f as remove
+# from hak.one.file.pickle.save import f as save
+# from hak.many.strings.filepaths.py.testables.get import f as make_Pi_t
+# from hak.one.dict.durations.to_tuple_list_sorted_by_duration import f as sort
+
+# def f(_Pi=None):
+#   _Pi = list_testables()
+#   try: prev = load_pickle('./last_modified.pickle') or set()
+#   except EOFError as _: remove('./last_modified.pickle'); prev = set()
+#   last_mods = {py_filename: getmtime(py_filename) for py_filename in _Pi}
+#   save(last_mods, './last_modified.pickle')
+#   _Pi_fail = set()
+#   _A = [_[0] for _ in sort(last_mods)[::-1]]
+#   _B = set(make_Pi_t(_Pi, True, prev, last_mods) + list(_Pi_fail))
+#   _Pi_t = [a for a in _A if a in _B]
+#   print(f'Oldest file: {_Pi_t[-1]}')
+
 def f(filepaths=None, term=None):
   term = term or Terminal()
   
@@ -113,7 +133,7 @@ def f(filepaths=None, term=None):
   last_mods = {py_filename: getmtime(py_filename) for py_filename in filepaths}
   save_pickle(last_mods, './last_modified.pickle')
 
-  python_filepaths_sorted_by_last_mod = [_[0] for _ in srt(last_mods)[::-1]]
+  python_filepaths_sorted_by_last_mod = [_[0] for _ in sort(last_mods)[::-1]]
   python_filepaths_to_test_and_previously_failed = make_Pi_t(
     filepaths,
     True,
