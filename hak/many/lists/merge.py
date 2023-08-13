afca7c385644c76fc706aa600a1c30bf75f2003c
John Forbes <john.robert.forbes@gmail.com>
2023-08-14 00:13:08 +1000

    Updated hak/many/lists/merge.py.

--- hak/many/lists/merge.py
+++ hak/many/lists/merge.py
@@ -1,7 +1,28 @@
-f = lambda _L_a, _L_b: _L_a + [_l_b for _l_b in _L_b if _l_b not in set(_L_a)]
+from hak.pxyz import f as pxyz
+from hak.pf import f as pf
 
-t = lambda: all([
-  [] == f([], []),
-  list('abcdef') == f(list('abc'), list('def')),
-  list('abcd') == f(list('abc'), list('bcd')),
-])
+f = lambda a, b: a + [b_i for b_i in b if b_i not in set(a)]
+
+def t_a():
+  x = {'a': [], 'b': []}
+  y = []
+  z = f(**x)
+  return pxyz(x, y, z)
+
+def t_b():
+  x = {'a': list('abc'), 'b': list('def')}
+  y = list('abcdef')
+  z = f(**x)
+  return pxyz(x, y, z)
+
+def t_c():
+  x = {'a': list('abc'), 'b': list('bcd')}
+  y = list('abcd')
+  z = f(**x)
+  return pxyz(x, y, z)
+
+def t():
+  if not t_a(): return pf('!t_a')
+  if not t_b(): return pf('!t_b')
+  if not t_c(): return pf('!t_c')
+  return True
