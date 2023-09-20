5830377068ce756516f6bff13650b802f7621bb2
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:23:42 +1000

    Updated hak/strings/date_pieces/get.py.

--- hak/strings/date_pieces/get.py
+++ hak/strings/date_pieces/get.py
@@ -1,31 +1,13 @@
 from hak.string.date.separator.get import f as get_separator
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # src.string.date_pieces.get
 # get_bag
 f = lambda x: set(x.split(get_separator(x)))
 
-def t_0():
-  x = '19 Nov 2021'
-  y = set(['19', 'Nov', '2021'])
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = '2021-11-19'
-  y = set(['2021', '11', '19'])
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_2():
-  x = '28/03/2022'
-  y = set(['2022', '03', '28'])
-  z = f(x)
-  return pxyz(x, y, z)
-
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
-  if not t_2(): return pf('t_2 failed')
-  return True
+  if not pxyf('19 Nov 2021', set([  '19', 'Nov', '2021']), f): return pf('!t_0')
+  if not pxyf( '2021-11-19', set(['2021',  '11',   '19']), f): return pf('!t_1')
+  if not pxyf( '28/03/2022', set(['2022',  '03',   '28']), f): return pf('!t_2')
+  return 1
