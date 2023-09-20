1914720902695434b0094834e66a5f162cae7699
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:10:46 +1000

    Updated hak/string/find_last_char.py.

--- hak/string/find_last_char.py
+++ hak/string/find_last_char.py
@@ -4,6 +4,4 @@ f = lambda string, char: len(string)-string[::-1].find(char)-1
 
 def t():
   x = {'string': 'a,b,c,de', 'char': ','}
-  y = 5
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, 5, f(**x))
