9a21d635edba22cb16ad4e3861977375c3c7f70c
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:42:33 +1000

    Updated hak/other/ternary.py.

--- hak/other/ternary.py
+++ hak/other/ternary.py
@@ -3,6 +3,6 @@ from hak.pf import f as pf
 f = lambda if_true, condition, if_false: if_true if condition else if_false
 
 def t():
-  if not f('a', 1, 'b') == 'a': return pf('t_true failed')
-  if not f('a', 0, 'b') == 'b': return pf('t_false failed')
+  if not f('a', 0, 'b') == 'b': return pf('t_0')
+  if not f('a', 1, 'b') == 'a': return pf('t_1')
   return 1
