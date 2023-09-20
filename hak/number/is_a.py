ada91f2f2a88824bdc09c120207599dfea13dbb4
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:41:43 +1000

    Updated hak/number/is_a.py.

--- hak/number/is_a.py
+++ hak/number/is_a.py
@@ -1,44 +1,14 @@
 from numbers import Number
 
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 f = lambda x: isinstance(x, Number)
 
-def t_true_zero():
-  x = 0
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_true_float():
-  x = 0.1
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_true_complex():
-  x = 0.1j + 5
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_str():
-  x = '0'
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_None():
-  x = None
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
 def t():
-  if not t_true_zero(): return pf('!t_true_zero')
-  if not t_true_float(): return pf('!t_true_float')
-  if not t_true_complex(): return pf('!t_true_complex')
-  if not t_false_str(): return pf('!t_false_str')
-  if not t_false_None(): return pf('!t_false_None')
-  return True
+  if not pxyf(None, 0, f): return pf('!t_false_None')
+  if not pxyf('0', 0, f): return pf('!t_false_str')
+  if not pxyf(0.1j + 5, 1, f): return pf('!t_true_complex')
+  if not pxyf(0.1, 1, f): return pf('!t_true_float')
+  if not pxyf(0, 1, f): return pf('!t_true_zero')
+  return 1
