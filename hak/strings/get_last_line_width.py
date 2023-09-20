7e97710db9007f0759e0c0ae764cec5954ac4593
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 21:25:09 +1000

    Updated hak/strings/get_last_line_width.py.

--- hak/strings/get_last_line_width.py
+++ hak/strings/get_last_line_width.py
@@ -1,116 +1,116 @@
 # ignore_overlength_lines
-from hak.string.char.last.find import f as find_last
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
+from hak.string.char.last.find import f as find_last
 
 # get_last_line_width
 f = lambda x: (len(x) - find_last(x, '\n')) if '\n' in x else len(x)
 
-def t_0():
-  x = "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 10"
-  y = 79
-  z = f(x)
-  return pxyz(x, y, z)
+t_0 = lambda: pxyf(
+  "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 10",
+  79,
+  f
+)
 
-def t_1():
-  x = '\n  '.join([
+t_1 = lambda: pxyf(
+  '\n  '.join([
     "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,",
     "'j': 10"
-  ])
-  y = 10
-  z = f(x)
-  return pxyz(x, y, z)
+  ]),
+  10,
+  f
+)
 
-def t_2():
-  x = "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10"
-  y = 87
-  z = f(x)
-  return pxyz(x, y, z)
+t_2 = lambda: pxyf(
+  "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10",
+  87,
+  f
+)
 
-def t_3():
-  x = '\n  '.join([
+t_3 = lambda: pxyf(
+  '\n  '.join([
     "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,",
     "'j': 9, 'k': 10"
-  ])
-  y = 18
-  z = f(x)
-  return pxyz(x, y, z)
+  ]),
+  18,
+  f
+)
 
-def t_4():
-  x = "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16"
-  y = 141
-  z = f(x)
-  return pxyz(x, y, z)
+t_4 = lambda: pxyf(
+  "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16",
+  141,
+  f
+)
 
-def t_5():
-  x = '\n  '.join([
+t_5 = lambda: pxyf(
+  '\n  '.join([
     "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,",
     "'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16"
-  ])
-  y = 72
-  z = f(x)
-  return pxyz(x, y, z)
+  ]),
+  72,
+  f
+)
 
-def t_6():
-  x = "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17"
-  y = 150
-  z = f(x)
-  return pxyz(x, y, z)
+t_6 = lambda: pxyf(
+  "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17",
+  150,
+  f
+)
 
-def t_7():
-  x = '\n  '.join([
+t_7 = lambda: pxyf(
+  '\n  '.join([
     "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,",
     "'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17"
-  ])
-  y = 81
-  z = f(x)
-  return pxyz(x, y, z)
+  ]),
+  81,
+  f
+)
 
-def t_8():
-  x = '\n  '.join([
+t_8 = lambda: pxyf(
+  '\n  '.join([
     "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,",
     "'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16,",
     "'r': 17"
-  ])
-  y = 10
-  z = f(x)
-  return pxyz(x, y, z)
+  ]),
+  10,
+  f
+)
 
-def t_9():
-  x = "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25"
-  y = 222
-  z = f(x)
-  return pxyz(x, y, z)
+t_9 = lambda: pxyf(
+  "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25",
+  222,
+  f
+)
 
-def t_a():
-  x = '\n  '.join([
+t_a = lambda: pxyf(
+  '\n  '.join([
     "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,",
     "'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25"
-  ])
-  y = 153
-  z = f(x)
-  return pxyz(x, y, z)
+  ]),
+  153,
+  f
+)
 
-def t_b():
-  x = '\n  '.join([
+t_b = lambda: pxyf(
+  '\n  '.join([
     "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,",
     "'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16,",
     "'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25"
-  ])
-  y = 82
-  z = f(x)
-  return pxyz(x, y, z)
+  ]),
+  82,
+  f
+)
 
-def t_c():
-  x = '\n  '.join([
+t_c = lambda: pxyf(
+  '\n  '.join([
     "'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8,",
     "'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16,",
     "'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24,",
     "'z': 25"
-  ])
-  y = 10
-  z = f(x)
-  return pxyz(x, y, z)
+  ]),
+  10,
+  f
+)
 
 def t():
   if not t_0(): return pf('!t_0')
@@ -126,4 +126,4 @@ def t():
   if not t_a(): return pf('!t_a')
   if not t_b(): return pf('!t_b')
   if not t_c(): return pf('!t_c')
-  return True
+  return 1
