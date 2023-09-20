2313297f824204089ada5d3aa489f6a8be937f48
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:38:44 +1000

    Updated hak/number/float/snap_to_zero.py.

--- hak/number/float/snap_to_zero.py
+++ hak/number/float/snap_to_zero.py
@@ -1,37 +1,13 @@
 from hak.number.float.epsilon import ε
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # src.float.snap_to_zero
 f = lambda x: 0 if abs(x) < 1e-10 else x
 
-def t_zero():
-  x = 0
-  y = 0
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_one():
-  x = 1
-  y = 1
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_negative_epsilon():
-  x = -ε
-  y = 0
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_positive_epsilon():
-  x = ε
-  y = 0
-  z = f(x)
-  return pxyz(x, y, z)
-
 def t():
-  if not t_zero(): return pf('t_zero failed')
-  if not t_one(): return pf('t_one failed')
-  if not t_negative_epsilon(): return pf('t_negative_epsilon failed')
-  if not t_positive_epsilon(): return pf('t_positive_epsilon failed')
-  return True
+  if not pxyf(-ε, 0, f): return pf('t_negative_epsilon failed')
+  if not pxyf( 1, 1, f): return pf('t_one failed')
+  if not pxyf( ε, 0, f): return pf('t_positive_epsilon failed')
+  if not pxyf( 0, 0, f): return pf('t_zero failed')
+  return 1
