de76344d3c5232b87babec847bb10cdf3155d1db
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 17:52:00 +1000

    Updated hak/dict/period/contains_date.py.

--- hak/dict/period/contains_date.py
+++ hak/dict/period/contains_date.py
@@ -4,22 +4,22 @@ from hak.dict.period.financial_year.contains_date import f as d_in_fy
 from hak.dict.period.financial_year.make import f as mkfy
 from hak.dict.period.month.contains_date import f as d_in_m
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # contains_d
 f = lambda x: (d_in_m if 'month' in x else d_in_fy)(x)
 
-def t_fy_true():
-  x = {'financial_year': mkfy({'start_year': 2022}), 'date': date(2022, 7, 5)}
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
+t_fy_true = lambda: pxyf(
+  {'financial_year': mkfy({'start_year': 2022}), 'date': date(2022, 7, 5)},
+  1,
+  f
+)
 
-def t_fy_false():
-  x = {'financial_year': mkfy({'start_year': 2022}), 'date': date(2022, 6, 5)}
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
+t_fy_false = lambda: pxyf(
+  {'financial_year': mkfy({'start_year': 2022}), 'date': date(2022, 6, 5)},
+  0,
+  f
+)
 
 def t_month():
   x = {'month': {'year': 2022, 'number': 1}, 'date': date(2022, 1, 10)}
@@ -31,4 +31,4 @@ def t():
   if not t_fy_true(): return pf('!t_fy_true()')
   if not t_fy_false(): return pf('!t_fy_false()')
   if not t_month(): return pf('!t_month()')
-  return True
+  return 1
