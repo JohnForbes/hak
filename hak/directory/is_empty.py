e316aedb939db566fed318f916e298b551aed964
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:27:34 +1000

    Updated hak/directory/is_empty.py.

--- hak/directory/is_empty.py
+++ hak/directory/is_empty.py
@@ -3,30 +3,20 @@ from hak.directory.make import f as mkdir
 from hak.directory.remove import f as rmdir
 from hak.pf import f as pf
 from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 f = lambda x: len(get_filepaths(root=x, filepaths=[])) <= 0
 
-def t_false():
-  x = './hak'
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def up():
-  x = './hak/directory/temp'
-  mkdir(x)
-  return x
-
+up = lambda: mkdir('./hak/directory/temp')
 dn = lambda x: rmdir(x)
 
 def t_true():
   x = up()
-  y = True
   z = f(x)
   dn(x)
-  return pxyz(x, y, z)
+  return pxyz(x, 1, z)
 
 def t():
-  if not t_false(): pf('!t_false')
+  if not pxyf('./hak', 0, f): pf('!t_false')
   if not t_true(): pf('!t_true')
-  return True
+  return 1
