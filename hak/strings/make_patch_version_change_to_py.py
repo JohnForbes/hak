ce6b00ed80af10c781151a44f2c0064d5e9df4ad
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 21:26:15 +1000

    Updated hak/strings/make_patch_version_change_to_py.py.

--- hak/strings/make_patch_version_change_to_py.py
+++ hak/strings/make_patch_version_change_to_py.py
@@ -1,7 +1,7 @@
 from hak.other.pip.version.to_str import f as make_v_str
-from hak.string.contains.version import f as k
-from hak.pxyz import f as pxyz
 from hak.pf import f as pf
+from hak.pxyz import f as pxyz
+from hak.string.contains.version import f as k
 
 f = lambda version, lines: [
   (f"  version='{make_v_str(version)}'," if k(l) else l) for l in lines
@@ -11,31 +11,23 @@ v = {'major': 4, 'minor': 5, 'patch': 6}
 
 def t_a():
   x = {'version': {}, 'lines': []}
-  y = []
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, [], f(**x))
 
 def t_b():
   x = {'version': v, 'lines': []}
-  y = []
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, [], f(**x))
 
 def t_c():
   x = {'version': v, 'lines': ["version = '1.2.3'\n", '.\n']}
-  y = ["  version='4.5.6',", '.\n']
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, ["  version='4.5.6',", '.\n'], f(**x))
 
 def t_d():
   x = {'version': v, 'lines': ['.\n', "version = '1.2.3'\n"]}
-  y = ['.\n', "  version='4.5.6',"]
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, ['.\n', "  version='4.5.6',"], f(**x))
 
 def t():
   if not t_a(): return pf('!t_a')
   if not t_b(): return pf('!t_b')
   if not t_c(): return pf('!t_c')
   if not t_d(): return pf('!t_d')
-  return True
+  return 1
