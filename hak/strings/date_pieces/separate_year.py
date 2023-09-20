e74b87e45191e5a83d3764b84dff7039afa693bd
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 21:13:10 +1000

    Updated hak/strings/date_pieces/separate_year.py.

--- hak/strings/date_pieces/separate_year.py
+++ hak/strings/date_pieces/separate_year.py
@@ -1,6 +1,6 @@
 from hak.pf import f as pf
+from hak.pxyf import f as pxyf
 from hak.string.year.is_a import f as is_year
-from hak.pxyz import f as pxyz
 
 # src.set.date_pieces.separate_year
 # separate_year_from_bag
@@ -14,26 +14,12 @@ def f(x):
       new_bag.add(item)
   return year, new_bag
 
-def t_0():
-  x = set(['19', 'Nov', '2021'])
-  y = 2021, set(['19', 'Nov'])
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = set(['2021', '11', '19'])
-  y = 2021, set(['11', '19'])
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_2():
-  x = set(['2022', '03', '28'])
-  y = 2022, set(['03', '28'])
-  z = f(x)
-  return pxyz(x, y, z)
+t_0 = lambda: pxyf(set([  '19', 'Nov', '2021']), (2021, set(['19', 'Nov'])), f)
+t_1 = lambda: pxyf(set(['2021',  '11',   '19']), (2021, set(['11',  '19'])), f)
+t_2 = lambda: pxyf(set(['2022',  '03',   '28']), (2022, set(['03',  '28'])), f)
 
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
-  if not t_2(): return pf('t_2 failed')
-  return True
+  if not t_0(): return pf('!t_0')
+  if not t_1(): return pf('!t_1')
+  if not t_2(): return pf('!t_2')
+  return 1
