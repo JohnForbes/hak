871a790409df8097cb40b88eeabe2c576b70f5ac
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 17:59:24 +1000

    Updated hak/dict/period/financial_year/get_start_date.py.

--- hak/dict/period/financial_year/get_start_date.py
+++ hak/dict/period/financial_year/get_start_date.py
@@ -2,24 +2,24 @@ from datetime import date
 
 from hak.dict.period.financial_year.make import f as mkfy
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # get_α_date
 f = lambda x: date(x['financial_year']['start_year'], 7, 1)
 
-def t_a():
-  x = {'financial_year': mkfy({'start_year': 2022})}
-  y = date(2022, 7, 1)
-  z = f(x)
-  return pxyz(x, y, z)
+t_a = lambda: pxyf(
+  {'financial_year': mkfy({'start_year': 2022})},
+  date(2022, 7, 1),
+  f
+)
 
-def t_b():
-  x = {'financial_year': mkfy({'final_year': 2022})}
-  y = date(2021, 7, 1)
-  z = f(x)
-  return pxyz(x, y, z)
+t_b = lambda: pxyf(
+  {'financial_year': mkfy({'final_year': 2022})},
+  date(2021, 7, 1),
+  f
+)
 
 def t():
   if not t_a(): return pf('!t_a')
   if not t_b(): return pf('!t_b')
-  return True
+  return 1
