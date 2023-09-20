670c3a55450c2869f67ee99ac05435e8740e9721
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:14:47 +1000

    Updated hak/string/month/is_a.py.

--- hak/string/month/is_a.py
+++ hak/string/month/is_a.py
@@ -1,56 +1,20 @@
 from hak.data.months import months
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # src.string.month.is_a
 # is_month
 def f(x):
-  x = x.lower()[:3]
   months_list = [m[:3].lower() for m in months]
-  if x in set(months_list): return True
-  return 1 <= int(x) <= 12 if x.isdecimal() else False
-
-def t_0():
-  x = 'Apr'
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = 'April'
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_2():
-  x = '04'
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_3():
-  x = '4'
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_4():
-  x = '0'
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_5():
-  x = '13'
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
+  x = x.lower()[:3]
+  if x in set(months_list): return 1
+  return 1 <= int(x) <= 12 if x.isdecimal() else 0
 
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
-  if not t_2(): return pf('t_2 failed')
-  if not t_3(): return pf('t_3 failed')
-  if not t_4(): return pf('t_4 failed')
-  if not t_5(): return pf('t_5 failed')
-  return True
+  if not pxyf(  'Apr', 1, f): return pf('t_0 failed')
+  if not pxyf('April', 1, f): return pf('t_1 failed')
+  if not pxyf(   '04', 1, f): return pf('t_2 failed')
+  if not pxyf(    '4', 1, f): return pf('t_3 failed')
+  if not pxyf(    '0', 0, f): return pf('t_4 failed')
+  if not pxyf(   '13', 0, f): return pf('t_5 failed')
+  return 1
