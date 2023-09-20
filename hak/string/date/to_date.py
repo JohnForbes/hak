6e54388ec2b78d31e640b0874836e2278580831b
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:07:45 +1000

    Updated hak/string/date/to_date.py.

--- hak/string/date/to_date.py
+++ hak/string/date/to_date.py
@@ -7,6 +7,7 @@ from hak.string.date.separator.get import f as get_separator
 from hak.string.month.to_number import f as to_number
 from hak.pf import f as pf
 from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # src.string.to_date
 def f(x, date_string_format=None):
@@ -26,45 +27,23 @@ def f(x, date_string_format=None):
     _ymd[date_string_format['day_index']]
   ]])
 
-def t_0():
-  x = '19 Nov 2021'
-  y = date(2021, 11, 19)
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = '2021-11-19'
-  y = date(2021, 11, 19)
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_2():
-  x = '28/03/2022'
-  y = date(2022, 3, 28)
-  z = f(x)
-  return pxyz(x, y, z)
-
 def t_3():
   x = '5/04/2022'
   x_date_string_format = {'year_index': 2, 'month_index': 1, 'day_index': 0}
-  y = date(2022, 4, 5)
-  z = f(x, x_date_string_format)
-  return pxyz(x, y, z)
+  return pxyz(x, date(2022, 4, 5), f(x, x_date_string_format))
 
 def t_4():
   x = '2022-05-06'
   x_date_string_format = {'year_index': 0, 'month_index': 1, 'day_index': 2}
-  y = date(2022, 5, 6)
-  z = f(x, x_date_string_format)
-  return pxyz(x, y, z)
+  return pxyz(x, date(2022, 5, 6), f(x, x_date_string_format))
 
 t_5 = lambda: f('2016-11-14') == date(2016, 11, 14)
 
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
-  if not t_2(): return pf('t_2 failed')
+  if not pxyf('19 Nov 2021', date(2021, 11, 19), f): return pf('t_0 failed')
+  if not pxyf('2021-11-19', date(2021, 11, 19), f): return pf('t_1 failed')
+  if not pxyf('28/03/2022', date(2022, 3, 28), f): return pf('t_2 failed')
   if not t_3(): return pf('t_3 failed')
   if not t_4(): return pf('t_4 failed')
   if not t_5(): return pf('t_5 failed')
-  return True
+  return 1
