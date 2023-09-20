579f50c9f5a6c4cf510aa80b3e0d0766311c0f9a
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:15:32 +1000

    Updated hak/string/month/to_number.py.

--- hak/string/month/to_number.py
+++ hak/string/month/to_number.py
@@ -1,6 +1,6 @@
 from hak.data.months import months
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # src.string.month.to_number
 # to_number
@@ -10,19 +10,7 @@ def f(x):
   if _x in months_list: return months_list.index(_x) + 1
   return int(_x)
 
-def t_0():
-  x = 'January'
-  y = 1
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = '12'
-  y = 12
-  z = f(x)
-  return pxyz(x, y, z)
-
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
-  return True
+  if not pxyf('January', 1, f): return pf('t_0 failed')
+  if not pxyf('12', 12, f): return pf('t_1 failed')
+  return 1
