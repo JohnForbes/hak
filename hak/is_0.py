6014bf77fa25d120246e06645e32067b30c7511e
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:40:14 +1000

    Updated hak/is_0.py.

--- hak/is_0.py
+++ hak/is_0.py
@@ -3,10 +3,8 @@ from hak.pxyf import f as pxyf
 
 # is_0
 f = lambda x: f"{x}" == '0'
-t_0 = lambda: pxyf(1, 0, f)
-t_1 = lambda: pxyf(0, 1, f)
 
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
+  if not pxyf(1, 0, f): return pf('!t_0')
+  if not pxyf(0, 1, f): return pf('!t_1')
   return 1
