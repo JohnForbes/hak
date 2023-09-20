1ea7ed60b313dcf3223d9f0e67cd1c632eabc5d1
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:33:19 +1000

    Updated hak/list/interleave.py.

--- hak/list/interleave.py
+++ hak/list/interleave.py
@@ -8,10 +8,5 @@ def f(list, item):
   return result[:-1]
 
 def t():
-  x = {
-    'list': [0, 1, 2, 3, 4, 5],
-    'item': 'A'
-  }
-  y = [0, 'A', 1, 'A', 2, 'A', 3, 'A', 4, 'A', 5]
-  z = f(**x)
-  return pxyz(x, y, z)
+  x = {'list': [0, 1, 2, 3, 4, 5], 'item': 'A'}
+  return pxyz(x, [0, 'A', 1, 'A', 2, 'A', 3, 'A', 4, 'A', 5], f(**x))
