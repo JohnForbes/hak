861b659d325fc882ae8811490962ff248ad48379
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 16:36:23 +1000

    Updated hak/bools/count_true.py.

--- hak/bools/count_true.py
+++ hak/bools/count_true.py
@@ -1,6 +1,7 @@
+from random import randint as u
+
 from hak.pxyf import f as pxyf
 from hak.pf import f as pf
-from random import randint as u
 
 f = lambda x: len([_ for _ in x if _])
 
@@ -12,10 +13,8 @@ def t_b():
   n = u(0, 10)
   return pxyf([*[1]*n, 0, False], n, f)
 
-t_c = lambda: pxyf([], 0, f)
-
 def t():
   if not t_a(): return pf('!t_a')
   if not t_b(): return pf('!t_b')
-  if not t_c(): return pf('!t_c')
-  return True
+  if not pxyf([], 0, f): return pf('!t_c')
+  return 1
