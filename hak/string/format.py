f5f585dfc702083f21aabaee1eb595e28d2ad666
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:11:54 +1000

    Updated hak/string/format.py.

--- hak/string/format.py
+++ hak/string/format.py
@@ -1,23 +1,23 @@
 # ignore_overlength_linesontain version
 from .append.new_line import f as append_new_line
+from .colour.bright.cyan import f as cy
+from .colour.bright.magenta import f as magenta
+from .colour.bright.yellow import f as yellow
+from .indent.run_if_class_method import f as indent_if_is_run_method
 from .insert.new_line_after_last_import_line import f as place_blank_line_after_last_import_line
 from .insert.new_line_before_comment import f as insert_nl_before_comment
 from .insert.new_line_before_def import f as insert_nl_before_def
+from .insert.new_line_before_if_main import f as insert_nl_before_if_main
 from .insert.new_line_before_lambda import f as add_nl_before_lambda_methods
 from .insert.new_line_before_lambda import f as insert_nl_before_lambda
-from .insert.new_line_before_if_main import f as insert_nl_before_if_main
+from .insert.new_line_before_new_line_and_cea_ import f as insert_nl_before_nl_and_cea_
 from .remove.empty_line_spaces import f as remove_empty_line_spaces
 from .remove.extra_new_line_following_class_definition import f as remove_gap_following_class_definition
 from .remove.first_new_line_if_starts_with_new_line import f as remove_first_nl_if_file_starts_with_newline
+from .remove.whitespace_between_newlines import f as remove_whitespace_between_newlines
 from .replace.double_new_line_with_single_new_line import f as replace_double_nl_with_single_new_line
 from .replace.single_new_line_with_empty_string import f as replace_single_nl_content_with_empty_string
 from .replace.triple_new_line_with_double_new_line import f as replace_triple_newlines_with_double_newlines
-from .insert.new_line_before_new_line_and_cea_ import f as insert_nl_before_nl_and_cea_
-from .indent.run_if_class_method import f as indent_if_is_run_method
-from .remove.whitespace_between_newlines import f as remove_whitespace_between_newlines
-from .colour.bright.yellow import f as yellow
-from .colour.bright.cyan import f as cy
-from .colour.bright.magenta import f as magenta
 from hak.strings.compare import f as compare_strings
 
 def f(x: str):
@@ -76,17 +76,17 @@ def t():
     '  fi = FakeInput(["y"])',
     '  try:',
     '    run(fi)',
-    '    return True',
+    '    return 1',
     '  except RuntimeError:',
-    '    return False',
+    '    return 0',
     '',
     'def t_do_not_proceed_path():',
     '  fi = FakeInput(["n"])',
     '  try:',
     '    run(fi)',
-    '    return False',
+    '    return 0',
     '  except RuntimeError:',
-    '    return True',
+    '    return 1',
     '',
     'test = lambda: all([',
     '  t_proceed_path(),',
@@ -128,17 +128,17 @@ def t():
     '  fi = FakeInput(["y"])',
     '  try:',
     '    run(fi)',
-    '    return True',
+    '    return 1',
     '  except RuntimeError:',
-    '    return False',
+    '    return 0',
     '',
     'def t_do_not_proceed_path():',
     '  fi = FakeInput(["n"])',
     '  try:',
     '    run(fi)',
-    '    return False',
+    '    return 0',
     '  except RuntimeError:',
-    '    return True',
+    '    return 1',
     '',
     'test = lambda: all([',
     '  t_proceed_path(),',
