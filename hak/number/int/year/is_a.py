9d7ed50a96844ebe9eb55d2f1afa87abd2c86fb6
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:41:24 +1000

    Updated hak/number/int/year/is_a.py.

--- hak/number/int/year/is_a.py
+++ hak/number/int/year/is_a.py
@@ -1,47 +1,17 @@
 from hak.number.int.is_a import f as is_int
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 def f(x):
-  if not is_int(x): return False
-  if x <= 0: return False
-  if x >= 10000: return False
-  return True
-
-def t_true():
-  x = 2023
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_neg():
-  x = -2023
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_float():
-  x = 2000.5
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_zero():
-  x = 0
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_too_big():
-  x = 10000
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
+  if not is_int(x): return 0
+  if x <= 0: return 0
+  if x >= 10000: return 0
+  return 1
 
 def t():
-  if not t_true(): return pf('!t_true')
-  if not t_false_neg(): return pf('!t_false_neg')
-  if not t_false_float(): return pf('!t_false_float')
-  if not t_false_zero(): return pf('!t_false_zero')
-  if not t_false_too_big(): return pf('!t_false_too_big')
-  return True
+  if not pxyf( 2023, 1, f): return pf('!t_true')
+  if not pxyf(-2023, 0, f): return pf('!t_false_neg')
+  if not pxyf(2000.5, 0, f): return pf('!t_false_float')
+  if not pxyf(    0, 0, f): return pf('!t_false_zero')
+  if not pxyf(10000, 0, f): return pf('!t_false_too_big')
+  return 1
