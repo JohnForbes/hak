b1e19b86349effb1618a1018cbecda18ae5669b7
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:13:14 +1000

    Updated hak/string/json/is_a.py.

--- hak/string/json/is_a.py
+++ hak/string/json/is_a.py
@@ -3,7 +3,7 @@ from json import dumps
 
 def f(x):
   try: loads(x)
-  except ValueError: return False
-  return True
+  except ValueError: return 0
+  return 1
 
 t = lambda: all([not f(''), f(dumps({'a': 0, 'b': 1}))])
