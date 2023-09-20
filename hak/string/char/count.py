4688b2f9fba5931aef37cb746dd8b343ae6b0fc3
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:58:09 +1000

    Updated hak/string/char/count.py.

--- hak/string/char/count.py
+++ hak/string/char/count.py
@@ -1,28 +1,22 @@
-from hak.pxyz import f as pxyz
 from hak.pf import f as pf
+from hak.pxyz import f as pxyz
 
 f = lambda string, char: len(string) - len(string.replace(char, ''))
 
 def t_a():
   x = {'string': 'a', 'char': ' ' }
-  y = 0
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, 0, f(**x))
 
 def t_b():
   x = {'string': 'a b', 'char': ' ' }
-  y = 1
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, 1, f(**x))
 
 def t_c():
   x = {'string': 'a b c', 'char': ' ' }
-  y = 2
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, 2, f(**x))
 
 def t():
   if not t_a(): return pf('!t_a')
   if not t_b(): return pf('!t_b')
   if not t_c(): return pf('!t_c')
-  return True
+  return 1
