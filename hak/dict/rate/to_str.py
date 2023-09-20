96d6f69a9e913fdf614119fbcfdd29f80797f605
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 18:56:22 +1000

    Updated hak/dict/rate/to_str.py.

--- hak/dict/rate/to_str.py
+++ hak/dict/rate/to_str.py
@@ -1,13 +1,9 @@
-from hak.dict.rate.make import f as make_rate
-from hak.dict.rate.make import f as make_rate
+from hak.dict.rate.make import f as mk_rate
+from hak.dict.rate.make import f as mk_rate
 from hak.dict.rate.to_float import f as to_float
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # __str__
 f = lambda x: f"{to_float(x):.2f}"
 
-def t():
-  x = make_rate(710, 113, {'a': 1})
-  y = '6.28'
-  z = f(x)
-  return pxyz(x, y, z)
+t = lambda: pxyf(mk_rate(710, 113, {'a': 1}), '6.28', f)
