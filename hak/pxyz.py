e16fbb0f7605350612167fc222ffcd968c844299
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:54:32 +1000

    Updated hak/pxyz.py.

--- hak/pxyz.py
+++ hak/pxyz.py
@@ -8,19 +8,18 @@ def f(x, y, z, p=print, new_line=False):
 def t_true():
   _fake_printer = FP()
   x = {'x': 1, 'y': 1, 'z': 1}
-  y = True
-  z = f(**x, p=_fake_printer)
-  return y == z and _fake_printer.history == []
+  return all([f(**x, p=_fake_printer), _fake_printer.history == []])
 
 def t_false():
   _fake_printer = FP()
   x = {'x': 1, 'y': 1, 'z': 2}
-  y = False
-  z = f(**x, p=_fake_printer)
-  return y == z and _fake_printer.history == ['x: 1\ny: 1\nz: 2']
+  return all([
+    not f(**x, p=_fake_printer),
+    _fake_printer.history == ['x: 1\ny: 1\nz: 2']
+  ])
 
 def t():
   # TODO: Insufficient test coverage: Skipped additional test for new line code
   if not t_true(): return pf('!t_true')
   if not t_false(): return pf('!t_false')
-  return True
+  return 1
