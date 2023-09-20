346b2d0fd9a8d8dcef4ef1e4e8313278875b1e20
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:55:00 +1000

    Updated hak/pyfiles/format.py.

--- hak/pyfiles/format.py
+++ hak/pyfiles/format.py
@@ -5,6 +5,4 @@ f = lambda x, function=format_one: [function(x_i) for x_i in x]
 
 def t():
   x = {'x': list('abc'), 'function': lambda x: x.capitalize()}
-  y = list('ABC')
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, list('ABC'), f(**x))
