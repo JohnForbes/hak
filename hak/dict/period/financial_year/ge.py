7474459e3581b9d42d97544879a09bbab6851064
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 17:55:48 +1000

    Updated hak/dict/period/financial_year/ge.py.

--- hak/dict/period/financial_year/ge.py
+++ hak/dict/period/financial_year/ge.py
@@ -8,21 +8,21 @@ f = lambda u, v: u['start_year'] >= v['start_year']
 def t_true_a():
   u = mkfy({'start_year': 2022})
   v = mkfy({'final_year': 2023})
-  y = True
+  y = 1
   z = f(u, v)
   return puvyz(u, v, y, z)
 
 def t_true_b():
   u = mkfy({'start_year': 2023})
   v = mkfy({'final_year': 2023})
-  y = True
+  y = 1
   z = f(u, v)
   return puvyz(u, v, y, z)
 
 def t_false():
   u = mkfy({'start_year': 2021})
   v = mkfy({'final_year': 2023})
-  y = False
+  y = 0
   z = f(u, v)
   return puvyz(u, v, y, z)
 
@@ -30,4 +30,4 @@ def t():
   if not t_true_a(): return pf('!t_true_a()')
   if not t_true_b(): return pf('!t_true_b()')
   if not t_false(): return pf('!t_false()')
-  return True
+  return 1
