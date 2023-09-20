a9dd9fbfc0cb785b019c5dd0cda4dc91f56562b4
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:36:59 +1000

    Updated hak/lists/merge.py.

--- hak/lists/merge.py
+++ hak/lists/merge.py
@@ -5,24 +5,18 @@ f = lambda a, b: a + [b_i for b_i in b if b_i not in set(a)]
 
 def t_a():
   x = {'a': [], 'b': []}
-  y = []
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, [], f(**x))
 
 def t_b():
   x = {'a': list('abc'), 'b': list('def')}
-  y = list('abcdef')
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, list('abcdef'), f(**x))
 
 def t_c():
   x = {'a': list('abc'), 'b': list('bcd')}
-  y = list('abcd')
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, list('abcd'), f(**x))
 
 def t():
   if not t_a(): return pf('!t_a')
   if not t_b(): return pf('!t_b')
   if not t_c(): return pf('!t_c')
-  return True
+  return 1
