1fef59ba0d58ccfd9feb2fbf345600b7fd7cc469
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 13:39:34 +1000

    Updated hak/other/pip/version/get.py.

--- hak/other/pip/version/get.py
+++ hak/other/pip/version/get.py
@@ -6,12 +6,12 @@ from hak.pxyz import f as pxyz
 def f(name, response=None):
   response = response or get(f"https://pypi.org/project/{name}/")
   lines = [l for l in response.text.split('\n') if all([
-    '<h2>Initiate a new hak project</h2>' not in l,
-    'hak ' in l,
+    f'<h2>Initiate a new {name} project</h2>' not in l,
+    f'{name} ' in l,
     'title' not in l
   ])]
   v_line = lines[0] if lines else '0.0.4'
-  v_str = v_line.split('hak ')[-1]
+  v_str = v_line.split(f'{name} ')[-1]
   return make_from_key_val_lists(
     ['major', 'minor', 'patch'],
     [int(_) for _ in v_str.split('.')]
