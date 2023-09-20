750a0ac7344ab11ab19565d7fbfde938788cddbf
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:50:28 +1000

    Updated hak/pf.py.

--- hak/pf.py
+++ hak/pf.py
@@ -2,28 +2,31 @@ from hak.fake.printer import f as FP
 
 # print_and_return_false
 def f(x, p=print, silent=False):
-  if silent: return False
+  if silent: return 0
   if isinstance(x, list): x = '\n'.join(x)
   p(x)
-  return False
+  return 0
 
 def t_0():
   _fake_printer = FP()
-  z = f('abc', _fake_printer)
-  return all([not z, _fake_printer.history[0] == 'abc'])
+  return all([not f('abc', _fake_printer), _fake_printer.history[0] == 'abc'])
 
 def t_1():
   _fake_printer = FP()
-  z = f(['abc', '123', 'xyz'], _fake_printer)
-  return all([not z, _fake_printer.history[0] == 'abc\n123\nxyz'])
+  return all([
+    not f(['abc', '123', 'xyz'], _fake_printer),
+    _fake_printer.history[0] == 'abc\n123\nxyz'
+  ])
 
 def t_silent():
   _fake_printer = FP()
-  z = f('abc', _fake_printer, silent=True)
-  return all([not z, _fake_printer.history == []])
+  return all([
+    not f('abc', _fake_printer, silent=True),
+    _fake_printer.history == []
+  ])
 
 def t():
-  if not t_0(): print("t_0() Failed"); return False
-  if not t_1(): print("t_1() Failed"); return False
-  if not t_silent(): print("t_silent() Failed"); return False
-  return True
+  if not t_0(): print("t_0() Failed"); return 0
+  if not t_1(): print("t_1() Failed"); return 0
+  if not t_silent(): print("t_silent() Failed"); return 0
+  return 1
