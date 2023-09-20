fe40fd7edfb2149dadabc8f082998cb3ad36c7ed
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 18:03:24 +1000

    Updated hak/dict/period/financial_year/is_a.py.

--- hak/dict/period/financial_year/is_a.py
+++ hak/dict/period/financial_year/is_a.py
@@ -1,54 +1,29 @@
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 def f(x):
-  if 'start_year' not in x: return False
-  if 'final_year' not in x: return False
-  if x['final_year'] - x['start_year'] != 1: return False
-  if len(x) != 2: return False
-  return True
+  if 'start_year' not in x: return 0
+  if 'final_year' not in x: return 0
+  if x['final_year'] - x['start_year'] != 1: return 0
+  if len(x) != 2: return 0
+  return 1
 
-def t_true():
-  x = {'start_year': 2022, 'final_year': 2023}
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
+t_0_missing_start_year = lambda: pxyf({'final_year': 2024}, 0, f)
+t_0_missing_final_year = lambda: pxyf({'start_year': 2022}, 0, f)
+t_0_years_too_far = lambda: pxyf({'start_year': 2022, 'final_year': 2024}, 0, f)
+t_0_years_same = lambda: pxyf({'start_year': 2022, 'final_year': 2022}, 0, f)
 
-def t_false_missing_start_year():
-  x = {'final_year': 2024}
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_missing_final_year():
-  x = {'start_year': 2022}
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_years_too_far():
-  x = {'start_year': 2022, 'final_year': 2024}
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_years_same():
-  x = {'start_year': 2022, 'final_year': 2022}
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false_has_extra_k():
-  x = {'start_year': 2022, 'final_year': 2023, 'extra_key': None}
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
+t_0_has_extra_k = lambda: pxyf(
+  {'start_year': 2022, 'final_year': 2023,
+  'extra_key': None}, 0,
+  f
+)
 
 def t():
-  if not t_true(): return pf('!t_true')
-  if not t_false_missing_start_year(): return pf('!t_false_missing_start_year')
-  if not t_false_missing_final_year(): return pf('!t_false_missing_final_year')
-  if not t_false_years_too_far(): return pf('!t_false_years_too_far')
-  if not t_false_years_same(): return pf('!t_false_years_same')
-  if not t_false_has_extra_k(): return pf('!t_false_has_extra_k')
-  return True
+  if not pxyf({'start_year': 2022, 'final_year': 2023}, 1, f): return pf('!t_1')
+  if not t_0_missing_start_year(): return pf('!t_0_missing_start_year')
+  if not t_0_missing_final_year(): return pf('!t_0_missing_final_year')
+  if not t_0_years_too_far(): return pf('!t_0_years_too_far')
+  if not t_0_years_same(): return pf('!t_0_years_same')
+  if not t_0_has_extra_k(): return pf('!t_0_has_extra_k')
+  return 1
