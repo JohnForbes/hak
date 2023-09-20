4c86d775883055b24eadbc702ba8d2ea601359db
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:06:50 +1000

    Updated hak/string/date/separator/get.py.

--- hak/string/date/separator/get.py
+++ hak/string/date/separator/get.py
@@ -1,5 +1,5 @@
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 from string import digits
 
 # src.string.date.separator.get
@@ -13,40 +13,10 @@ def f(x):
     if d[k] == 2:
       return k
 
-def t_0():
-  x = '2022-11-11'
-  y = '-'
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = '28/03/2022'
-  y = '/'
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_2():
-  x = '2022 01 31'
-  y = ' '
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_3():
-  x = '19 Nov 2021'
-  y = ' '
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_4():
-  x = '2022-05-06'
-  y = '-'
-  z = f(x)
-  return pxyz(x, y, z)
-
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
-  if not t_2(): return pf('t_2 failed')
-  if not t_3(): return pf('t_3 failed')
-  if not t_4(): return pf('t_4 failed')
-  return True
+  if not pxyf( '2022-11-11', '-', f): return pf('t_0 failed')
+  if not pxyf( '28/03/2022', '/', f): return pf('t_1 failed')
+  if not pxyf( '2022 01 31', ' ', f): return pf('t_2 failed')
+  if not pxyf('19 Nov 2021', ' ', f): return pf('t_3 failed')
+  if not pxyf( '2022-05-06', '-', f): return pf('t_4 failed')
+  return 1
