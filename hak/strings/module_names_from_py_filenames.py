d726236d9f037df462df2f7a3f8512e984bb9a1c
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 21:26:56 +1000

    Updated hak/strings/module_names_from_py_filenames.py.

--- hak/strings/module_names_from_py_filenames.py
+++ hak/strings/module_names_from_py_filenames.py
@@ -1,29 +1,17 @@
 from hak.string.filename.to_module_name import f as get_module_name
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 f = lambda x: sorted([get_module_name(x_i) for x_i in x])
 
-def t_a():
-  x = []
-  y = []
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_b():
-  x = ['./abc/xyz.py']
-  y = ['abc.xyz']
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_c():
-  x = ['./abc/xyz.py', './abc/mno/xyz.py']
-  y = ['abc.mno.xyz', 'abc.xyz']
-  z = f(x)
-  return pxyz(x, y, z)
+t_c = lambda: pxyf(
+  ['./abc/xyz.py', './abc/mno/xyz.py'],
+  ['abc.mno.xyz', 'abc.xyz'],
+  f
+)
 
 def t():
-  if not t_a(): return pf('!t_a')
-  if not t_b(): return pf('!t_b')
+  if not pxyf([], [], f): return pf('!t_a')
+  if not pxyf(['./abc/xyz.py'], ['abc.xyz'], f): return pf('!t_b')
   if not t_c(): return pf('!t_c')
-  return True
+  return 1
