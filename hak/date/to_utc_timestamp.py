c9e8ffc086aec4a569082abfc082f7049c658be7
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 16:44:36 +1000

    Updated hak/date/to_utc_timestamp.py.

--- hak/date/to_utc_timestamp.py
+++ hak/date/to_utc_timestamp.py
@@ -1,29 +1,13 @@
 from datetime import date
 from datetime import datetime as dt
-from hak.pf import f as pf
-from hak.pxyz import f as pxyz
 from time import timezone
+
+from hak.pf import f as pf
+from hak.pxyf import f as pxyf
 
 f = lambda x: dt(x.year, x.month, x.day).timestamp() - timezone
 
-def t_0():
-  x = date(1970,1,1)
-  y = 0
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = date(1970,1,2)
-  y = 86400
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_2():
-  x = date(2022,3,4)
-  y = 1646352000
-  z = f(x)
-  return pxyz(x, y, z)
-
 def t():
-  if not t_0(): return pf('t_0 failed')
-  return True
+  if not pxyf(date(1970,1,1),     0, f): return pf('!t_a')
+  if not pxyf(date(1970,1,2), 86400, f): return pf('!t_b')
+  return 1
