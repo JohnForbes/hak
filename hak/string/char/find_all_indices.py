81907f4b1efd1d613d6b218b3f7fc6b419c0d745
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:58:41 +1000

    Updated hak/string/char/find_all_indices.py.

--- hak/string/char/find_all_indices.py
+++ hak/string/char/find_all_indices.py
@@ -4,6 +4,4 @@ f = lambda string, char: [i for (i, c) in enumerate(string) if c == char]
 
 def t():
   x = {'string': 'a,b,c,defg', 'char': ','}
-  y = [1, 3, 5]
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, [1, 3, 5], f(**x))
