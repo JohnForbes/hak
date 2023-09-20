83a801ceda88b0f49ff461af56b091484dc59710
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 21:33:50 +1000

    Updated hak/tuples/to_dict.py.

--- hak/tuples/to_dict.py
+++ hak/tuples/to_dict.py
@@ -1,4 +1,4 @@
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # convert_list_of_tuples_to_dict
 def f(x):
@@ -6,8 +6,4 @@ def f(x):
   for (k, v) in x: Δ[k] += v
   return Δ
 
-def t():
-  x = [('a', 1), ('b', 2), ('a', 3), ('b', 4)]
-  y = {'a': 4, 'b': 6}
-  z = f(x)
-  return pxyz(x, y, z)
+t = lambda: pxyf([('a', 1), ('b', 2), ('a', 3), ('b', 4)], {'a': 4, 'b': 6}, f)
