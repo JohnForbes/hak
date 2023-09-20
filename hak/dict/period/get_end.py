b411c35bfd8d6a41710ce8bc9f3f139dcdf54d21
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 18:14:04 +1000

    Updated hak/dict/period/get_end.py.

--- hak/dict/period/get_end.py
+++ hak/dict/period/get_end.py
@@ -7,46 +7,23 @@ from .financial_year.make import f as mkfy
 from .month.get_end_date import f as get_col_hor_line_from_records
 
 from hak.pf import f as pf
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
+from hak.dict.period.month.make import f as mk_month
 
 # get_ω
 f = lambda x: (get_col_hor_line_from_records if 'month' in x else f_fy)(x)
 
-def t_fy():
-  x = {'financial_year': mkfy({'start_year': 2022})}
-  y = date(2023, 6, 30)
-  z = f(x)
-  return pxyz(x, y, z)
+t_fy = lambda: pxyf(
+  {'financial_year': mkfy({'start_year': 2022})},
+  date(2023, 6, 30),
+  f
+)
 
-def t_0():
-  x = {'month': {'year': 2016, 'number':  5}}
-  y = date(2016,  5, 31)
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_1():
-  x = {'month': {'year': 2017, 'number':  6}}
-  y = date(2017,  6, 30)
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_2():
-  x = {'month': {'year': 2020, 'number':  2}}
-  y = date(2020,  2, 29)
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_3():
-  x = {'month': {'year': 2021, 'number':  2}}
-  y = date(2021,  2, 28)
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_4():
-  x = {'month': {'year': 2021, 'number': 12}}
-  y = date(2021, 12, 31)
-  z = f(x)
-  return pxyz(x, y, z)
+t_0 = lambda: pxyf({'month': mk_month(2016,  5)}, date(2016,  5, 31), f)
+t_1 = lambda: pxyf({'month': mk_month(2017,  6)}, date(2017,  6, 30), f)
+t_2 = lambda: pxyf({'month': mk_month(2020,  2)}, date(2020,  2, 29), f)
+t_3 = lambda: pxyf({'month': mk_month(2021,  2)}, date(2021,  2, 28), f)
+t_4 = lambda: pxyf({'month': mk_month(2021, 12)}, date(2021, 12, 31), f)
 
 def t():
   if not t_0(): return pf('!t_0')
@@ -55,4 +32,4 @@ def t():
   if not t_3(): return pf('!t_3')
   if not t_4(): return pf('!t_4')
   if not t_fy(): return pf('!t_fy')
-  return True
+  return 1
