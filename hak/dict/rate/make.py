c0a463d7bbca06a7f36fd19f72e5bbfa898e96a0
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 18:41:13 +1000

    Updated hak/dict/rate/make.py.

--- hak/dict/rate/make.py
+++ hak/dict/rate/make.py
@@ -60,56 +60,51 @@ def f(numerator, denominator, unit):
     dpf = get_prime_factors(denominator)
     common_factors = set(npf.keys()).intersection(set(dpf.keys()))
 
-  result = {'numerator': numerator, 'denominator': denominator, 'unit': unit}
-  return result
+  return {'numerator': numerator, 'denominator': denominator, 'unit': unit}
 
 def t_a():
   x = {'numerator': 10, 'denominator': 20, 'unit': {'a': 1}}
-  y = {'numerator':  1, 'denominator':  2, 'unit': {'a': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, {'numerator':  1, 'denominator':  2, 'unit': {'a': 1}}, f(**x))
 
 def t_b():
   x = {'numerator': 0.1, 'denominator': 0.2, 'unit': {'b': 1}}
-  y = {'numerator': 1, 'denominator': 2, 'unit': {'b': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, {'numerator': 1, 'denominator': 2, 'unit': {'b': 1}}, f(**x))
 
 def t_c():
   x = {'numerator': 100, 'denominator': 4, 'unit': {'c': 1}}
-  y = {'numerator':  25, 'denominator': 1, 'unit': {'c': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, {'numerator':  25, 'denominator': 1, 'unit': {'c': 1}}, f(**x))
 
 def t_d():
   x = {'numerator': 25.0, 'denominator': 1, 'unit': {'d': 1}}
-  y = {'numerator': 25, 'denominator': 1, 'unit': {'d': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, {'numerator': 25, 'denominator': 1, 'unit': {'d': 1}}, f(**x))
 
 def t_e():
   x = {'numerator': 80, 'denominator': 4, 'unit': {'e': 1}}
-  y = {'numerator': 20, 'denominator': 1, 'unit': {'e': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, {'numerator': 20, 'denominator': 1, 'unit': {'e': 1}}, f(**x))
 
 def t_f():
   x = {'numerator': 0.7093094658085993, 'denominator': 1, 'unit': {'f': 1}}
-  y = {'numerator': 7093094658085993, 'denominator': 10**16, 'unit': {'f': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(
+    x,
+    {'numerator': 7093094658085993, 'denominator': 10**16, 'unit': {'f': 1}},
+    f(**x)
+  )
 
 def t_g():
   x = {'numerator': 5472.0, 'denominator': 7350.89, 'unit': {'g': 1}}
-  y = {'numerator': 547200, 'denominator': 735089, 'unit': {'g': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(
+    x,
+    {'numerator': 547200, 'denominator': 735089, 'unit': {'g': 1}},
+    f(**x)
+  )
 
 def t_h():
   x = {'numerator': 4491.36, 'denominator': 48, 'unit': {'h': 1}}
-  y = {'numerator': 9357, 'denominator': 100, 'unit': {'h': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(
+    x,
+    {'numerator': 9357, 'denominator': 100, 'unit': {'h': 1}},
+    f(**x)
+  )
 
 def t_i():
   x = {
@@ -117,25 +112,23 @@ def t_i():
     'denominator':  2500000000000000,
     'unit': {'i': 1}
   }
-  y = {
-    'numerator':     658678432894737,
-    'denominator':      500000000000,
-    'unit': {'i': 1}
-  }
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(
+    x,
+    {
+      'numerator':     658678432894737,
+      'denominator':      500000000000,
+      'unit': {'i': 1}
+    },
+    f(**x)
+  )
 
 def t_j():
   x = {'numerator': 0, 'denominator': 10, 'unit': {'j': 1}}
-  y = {'numerator': 0, 'denominator':  1, 'unit': {'j': 1}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, {'numerator': 0, 'denominator':  1, 'unit': {'j': 1}}, f(**x))
 
 def t_k():
   x = {'numerator': 0, 'denominator': 10, 'unit': {}}
-  y = {'numerator': 0, 'denominator':  1, 'unit': {}}
-  z = f(**x)
-  return pxyz(x, y, z)
+  return pxyz(x, {'numerator': 0, 'denominator':  1, 'unit': {}}, f(**x))
 
 def t():
   if not t_a(): return pf('!t_a')
@@ -149,4 +142,4 @@ def t():
   if not t_i(): return pf('!t_i')
   if not t_j(): return pf('!t_j')
   if not t_k(): return pf('!t_k')
-  return True
+  return 1
