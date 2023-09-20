c030bf8f77a78e412a6ff60293dabfdf981f4258
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 18:34:02 +1000

    Updated hak/dict/rate/is_a.py.

--- hak/dict/rate/is_a.py
+++ hak/dict/rate/is_a.py
@@ -1,34 +1,18 @@
 from hak.pf import f as pf
-from hak.dict.rate.make import f as make_rate
-from hak.pxyz import f as pxyz
+from hak.dict.rate.make import f as mk_rate
+from hak.pxyf import f as pxyf
 
 def f(x):
-  if not isinstance(x, dict): return False
-  if not 'numerator' in x: return False
-  if not 'denominator' in x: return False
-  if not 'unit' in x: return False
-  return True
+  if not isinstance(x, dict): return 0
+  if not 'numerator' in x: return 0
+  if not 'denominator' in x: return 0
+  if not 'unit' in x: return 0
+  return 1
 
-def t_true():
-  x = make_rate(1, 2, {})
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_true_units():
-  x = make_rate(1, 2, {'m': 2, 's': -1})
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false():
-  x = 'abc'
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
+t_true_units = lambda: pxyf(mk_rate(1, 2, {'m': 2, 's': -1}), 1, f)
 
 def t():
-  if not t_true(): return pf('!t_true()')
+  if not pxyf(mk_rate(1, 2, {}), 1, f): return pf('!t_true()')
   if not t_true_units(): return pf('!t_true_units()')
-  if not t_false(): return pf('!t_false()')
-  return True
+  if not pxyf('abc', 0, f): return pf('!t_false()')
+  return 1
