4f4ad441cc1f9a79f8b131b708e90e107c85a231
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 18:11:33 +1000

    Updated hak/dict/period/financial_year/to_str.py.

--- hak/dict/period/financial_year/to_str.py
+++ hak/dict/period/financial_year/to_str.py
@@ -1,11 +1,7 @@
 from hak.dict.period.financial_year.make import f as mkfy
-from hak.pxyz import f as pxyz
+from hak.pxyf import f as pxyf
 
 # to_str
 f = lambda x: f"{x['start_year']} - {x['final_year']}"
 
-def t():
-  x = mkfy({'start_year': 2022})
-  y = '2022 - 2023'
-  z = f(x)
-  return pxyz(x, y, z)
+t = lambda: pxyf(mkfy({'start_year': 2022}), '2022 - 2023', f)
