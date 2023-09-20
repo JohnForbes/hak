464f2b1077dd3eb7e92f5fc20ba54e9ae0d6eafe
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 21:30:45 +1000

    Updated hak/test/final_line/check.py.

--- hak/test/final_line/check.py
+++ hak/test/final_line/check.py
@@ -1,10 +1,10 @@
 from hak.file.load import f as load
 from hak.file.remove import f as remove
 from hak.file.save import f as save_file
-from hak.strings.filepaths.py.testables.get import f as list_testables
+from hak.pf import f as pf
 from hak.string.colour.bright.green import f as success
 from hak.string.colour.bright.red import f as danger
-from hak.pf import f as pf
+from hak.strings.filepaths.py.testables.get import f as list_testables
 from hak.terminal import Terminal
 
 # check_final_line
@@ -19,7 +19,7 @@ def f(filepaths=None, term=None):
     l = _lines[_last_index]
     if l != '': return pf([f'{pi}:{_last_index}', danger([l])], p=term.print)
   term.print(f"{success('PASS')} Final lines "+' '*20)
-  return True
+  return 1
 
 def t_expected_false():
   _test_files = {
@@ -30,7 +30,7 @@ def t_expected_false():
   for (k, v) in _test_files.items(): save_file(k, v)
   
   x = list(_test_files.keys())
-  y = False
+  y = 0
   y_out_stream_list = [
     'Checking final lines...\r',
     "./a.pz:1\n\x1b[1;31m['xyz']\x1b[0;0m\n"
@@ -46,7 +46,7 @@ def t_expected_false():
     f'term.output_stream_as_list: {term.output_stream_as_list}',
     f'y_out_stream_list:          {y_out_stream_list}',
   ])
-  return True
+  return 1
 
 def t_expected_true():
   _test_files = {
@@ -57,7 +57,7 @@ def t_expected_true():
   for (k, v) in _test_files.items(): save_file(k, v)
   
   x = list(_test_files.keys())
-  y = True
+  y = 1
   y_out_stream_list = [
     'Checking final lines...\r',
     '\x1b[1;32mPASS\x1b[0;0m Final lines                     \n'
@@ -73,9 +73,9 @@ def t_expected_true():
     f'term.output_stream_as_list: {term.output_stream_as_list}',
     f'y_out_stream_list:          {y_out_stream_list}',
   ])
-  return True
+  return 1
 
 def t():
   if not t_expected_true(): return pf('not t_expected_true()')
   if not t_expected_false(): return pf('not t_expected_false()')
-  return True
+  return 1
