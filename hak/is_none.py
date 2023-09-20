6b272c27be0a33c870edecb98f1b8cca6b322e48
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 20:41:55 +1000

    Updated hak/is_none.py.

--- hak/is_none.py
+++ hak/is_none.py
@@ -3,10 +3,8 @@ from hak.pxyf import f as pxyf
 
 # is_none
 f = lambda x: x is None
-t_1 = lambda: pxyf(None, 1, f)
-t_0 = lambda: pxyf(   0, 0, f)
 
 def t():
-  if not t_0(): return pf('t_0 failed')
-  if not t_1(): return pf('t_1 failed')
+  if not pxyf(   0, 0, f): return pf('!t_0')
+  if not pxyf(None, 1, f): return pf('!t_1')
   return 1
