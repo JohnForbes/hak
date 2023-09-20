666e94ff080287438787ec97db4bf0b9e6ab9761
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 17:34:37 +1000

    Updated hak/dict/get_sorted_keys.py.

--- hak/dict/get_sorted_keys.py
+++ hak/dict/get_sorted_keys.py
@@ -1,23 +1,14 @@
-from hak.pf import f as pf
-from hak.pxyz import f as pxyz
 from datetime import date
+
 from hak.dict.is_a import f as is_dict
+from hak.pf import f as pf
+from hak.pxyf import f as pxyf
 
 f = lambda x: sorted(x.keys()) if is_dict(x) else []
 
-def t_valid():
-  x = {'z': 0, 'y': 1, 'x': 2}
-  y = ['x', 'y', 'z']
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_date():
-  x = date.today()
-  y = []
-  z = f(x)
-  return pxyz(x, y, z)
+t_valid = lambda: pxyf({'z': 0, 'y': 1, 'x': 2}, ['x', 'y', 'z'], f)
 
 def t():
   if not t_valid(): return pf('t_valid failed')
-  if not t_date(): return pf('t_date failed')
-  return True
+  if not pxyf(date.today(), [], f): return pf('t_date failed')
+  return 1
