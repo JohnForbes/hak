e517ac0536c4e844f20e6b60b9f528148bef1791
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 21:06:48 +1000

    Updated hak/string/day/is_a.py.

--- hak/string/day/is_a.py
+++ hak/string/day/is_a.py
@@ -6,12 +6,12 @@ from hak.pxyf import f as pxyf
 f = lambda x: 1 <= int(x) <= 31 if x.isdecimal() else False
 
 def t():
-  if not pxyf(  'Apr', 0, f): return pf('t_0 failed')
-  if not pxyf('April', 0, f): return pf('t_1 failed')
-  if not pxyf(   '04', 1, f): return pf('t_2 failed')
-  if not pxyf(    '4', 1, f): return pf('t_3 failed')
-  if not pxyf(    '0', 0, f): return pf('t_4 failed')
-  if not pxyf(   '13', 1, f): return pf('t_5 failed')
-  if not pxyf(   '31', 1, f): return pf('t_6 failed')
-  if not pxyf(   '32', 0, f): return pf('t_7 failed')
+  if not pxyf(  'Apr', 0, f): return pf('!t_0')
+  if not pxyf('April', 0, f): return pf('!t_1')
+  if not pxyf(   '04', 1, f): return pf('!t_2')
+  if not pxyf(    '4', 1, f): return pf('!t_3')
+  if not pxyf(    '0', 0, f): return pf('!t_4')
+  if not pxyf(   '13', 1, f): return pf('!t_5')
+  if not pxyf(   '31', 1, f): return pf('!t_6')
+  if not pxyf(   '32', 0, f): return pf('!t_7')
   return 1
