a6aa4ca135609cce8d9ee8c1a65055f4b2b3111c
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 19:38:24 +1000

    Updated hak/number/float/random/make.py.

--- hak/number/float/random/make.py
+++ hak/number/float/random/make.py
@@ -1,4 +1,5 @@
 from random import random as u
+
 from hak.number.float.is_a import f as is_float
 from hak.pf import f as pf
 
@@ -9,4 +10,4 @@ def t():
   if not is_float(z): return pf('!is_float(z)')
   if z < 0: return pf('!z<0')
   if z > 1: return pf('!z>1')
-  return True
+  return 1
