2b3b9c581ff10240c72f00c683254804298cc2d6
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 16:41:31 +1000

    Updated hak/date/public_holiday/is_a.py.

--- hak/date/public_holiday/is_a.py
+++ hak/date/public_holiday/is_a.py
@@ -2,29 +2,28 @@ from datetime import date
 from datetime import timedelta as td
 
 from hak.date.is_monday import f as is_monday
+from hak.date.is_sunday import f as is_sunday
 from hak.date.year.easter_sunday.get import f as get_easter_sunday
 from hak.pf import f as pf
 from hak.pxyf import f as pxyf
 
-is_sunday = lambda δ: δ.weekday() == 6
-
 def f(x):
   easter = get_easter_sunday(x.year)
-  if x.month == 1 and x.day == 1: return True
-  if all([is_sunday(x-td(1)), x.month == 1, x.day == 2]): return True
-  if x.month == 1 and x.day == 26: return True
-  if x.month == 3 and x.day == 13: return True
-  if easter -td(2) == x: return True
-  if easter -td(1) == x: return True
-  if easter == x: return True
-  if easter +td(1) == x: return True
-  if x.month == 4 and x.day == 25: return True
-  if all([x.month == 5, is_monday(x), x.day >= 27]): return True
-  if all([x.month == 6, is_monday(x), 7 < x.day <= 14]): return True
-  if all([x.month == 10, is_monday(x), x.day <= 7]): return True
-  if all([x.month == 12, x.day == 25]): return True
-  if all([x.month == 12, x.day == 26]): return True
-  return False
+  if x.month == 1 and x.day == 1: return 1
+  if all([is_sunday(x-td(1)), x.month == 1, x.day == 2]): return 1
+  if x.month == 1 and x.day == 26: return 1
+  if x.month == 3 and x.day == 13: return 1
+  if easter -td(2) == x: return 1
+  if easter -td(1) == x: return 1
+  if easter == x: return 1
+  if easter +td(1) == x: return 1
+  if x.month == 4 and x.day == 25: return 1
+  if all([x.month == 5, is_monday(x), x.day >= 27]): return 1
+  if all([x.month == 6, is_monday(x), 7 < x.day <= 14]): return 1
+  if all([x.month == 10, is_monday(x), x.day <= 7]): return 1
+  if all([x.month == 12, x.day == 25]): return 1
+  if all([x.month == 12, x.day == 26]): return 1
+  return 0
 
 # Wed 25 Jan 2023
 t_not_public_holiday_a = lambda: pxyf(date(2023,  1, 25), 0, f)
@@ -71,10 +70,10 @@ t_not_public_holiday_e = lambda: pxyf(date(2023,  6, 19), 0, f)
 # Sovereign's Birthday	Monday 12 June 2023
 t_sovereigns_birthday  = lambda: pxyf(date(2023,  6, 12), 1, f)
 
-t_not_public_holiday_f = lambda: pxyf(date(2023, 10, 9), 0, f)
+t_not_public_holiday_f = lambda: pxyf(date(2023, 10,  9), 0, f)
 
 # Labour Day	Monday 2 October 2023
-t_labour_day           = lambda: pxyf(date(2023, 10, 2), 1, f)
+t_labour_day           = lambda: pxyf(date(2023, 10,  2), 1, f)
 
 # Christmas Day	Monday 25 December 2023
 t_christmas_day        = lambda: pxyf(date(2023, 12, 25), 1, f)
@@ -103,4 +102,4 @@ def t():
   if not t_labour_day(): return pf('t_labour_day failed')
   if not t_christmas_day(): return pf('t_christmas_day failed')
   if not t_boxing_day(): return pf('t_boxing_day failed')
-  return True
+  return 1
