449436d83987072c7db212e9ee50d70161d6e58f
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 18:20:29 +1000

    Updated hak/dict/rate/abs.py.

--- hak/dict/rate/abs.py
+++ hak/dict/rate/abs.py
@@ -1,24 +1,15 @@
 from hak.pf import f as pf
-from hak.dict.rate.make import f as make_rate
-from hak.pxyz import f as pxyz
+from hak.dict.rate.make import f as mk_rate
+from hak.pxyf import f as pxyf
 
 def f(x):
   if not isinstance(x, dict): raise ValueError(f'x: {x} is not a dict')
-  return make_rate(abs(x['numerator']), abs(x['denominator']), x['unit'])
+  return mk_rate(abs(x['numerator']), abs(x['denominator']), x['unit'])
 
-def t_a():
-  x = make_rate(-1, 3, {'a': 1})
-  y = make_rate( 1, 3, {'a': 1})
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_b():
-  x = make_rate(45, -7, {'b': 1})
-  y = make_rate(45,  7, {'b': 1})
-  z = f(x)
-  return pxyz(x, y, z)
+t_a = lambda: pxyf(mk_rate(-1,  3, {'a': 1}), mk_rate( 1, 3, {'a': 1}), f)
+t_b = lambda: pxyf(mk_rate(45, -7, {'b': 1}), mk_rate(45, 7, {'b': 1}), f)
 
 def t():
   if not t_a(): return pf('!t_a')
   if not t_b(): return pf('!t_b')
-  return True
+  return 1
