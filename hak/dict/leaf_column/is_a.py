81171d3cedd4bb28ea3df93429140b217caf26a0
John Forbes <john.robert.forbes@gmail.com>
2023-09-20 17:40:04 +1000

    Updated hak/dict/leaf_column/is_a.py.

--- hak/dict/leaf_column/is_a.py
+++ hak/dict/leaf_column/is_a.py
@@ -1,33 +1,23 @@
 from hak.dict.is_a import f as is_dict
-from hak.pf import f as pf
 from hak.list.is_a import f as is_list
-from hak.pxyz import f as pxyz
+from hak.pf import f as pf
+from hak.pxyf import f as pxyf
 
 def f(x):
-  if not is_dict(x): return False
-  if 'header' not in x: return False
-  if 'unit'   not in x: return False
-  if 'values' not in x: return False
-  if not is_list(x['values']): return False
-  return True
+  if not is_dict(x): return 0
+  if 'header' not in x: return 0
+  if 'unit'   not in x: return 0
+  if 'values' not in x: return 0
+  if not is_list(x['values']): return 0
+  return 1
 
-def t_true():
-  x = {
-    'header': 'apples',
-    'unit': '$/apple',
-    'values': [0.25, 0.75]
-  }
-  y = True
-  z = f(x)
-  return pxyz(x, y, z)
-
-def t_false():
-  x = None
-  y = False
-  z = f(x)
-  return pxyz(x, y, z)
+t_true = lambda: pxyf({
+  'header': 'apples',
+  'unit': '$/apple',
+  'values': [0.25, 0.75]
+}, 1, f)
 
 def t():
   if not t_true(): return pf('!t_true')
-  if not t_false(): return pf('!t_false')
-  return True
+  if not pxyf(None, 0, f): return pf('!t_false')
+  return 1
