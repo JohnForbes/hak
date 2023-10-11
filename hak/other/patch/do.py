84099ac0ad23428830e3370c771005c7758abd5d
John Forbes <john.robert.forbes@gmail.com>
2023-10-11 22:04:19 +1100

    Updated hak/other/patch/do.py.

--- hak/other/patch/do.py
+++ hak/other/patch/do.py
@@ -20,6 +20,27 @@ from hak.other.setup.py.update import f as update_setup_py
 from hak.other.setup.py.update import t as t_update_setup_py
 from subprocess import run as sprun
 
+def f(x):
+  x = deepcopy(x)
+  z = {}
+  z['v'] = x['v'] if 'v' in x else None
+  z['root'] = x['root'] if 'root' in x else '.'
+  z['cfg_path'] = f'{z["root"]}/setup.cfg'
+  z['py_path'] = f'{z["root"]}/setup.py'
+
+  z['v'] = z['v'] or get_pip_version('hak')
+  z['v']['patch'] += 1
+
+  update_setup_cfg({'v': z['v'], 'filename': z['cfg_path']})
+  update_setup_py({'v': z['v'], 'filename': z['py_path']})
+  empty_directory('./dist/')
+  
+  make_new_dist_tar(x)
+  # add_to_git(cwd=_root, cap_out=True)
+  if 'local_test_only' not in x: z['upload_result'] = start_upload(x)
+  return z
+
+
 def up():
   x = {}
   x['root'] = f"./hak_test"
@@ -87,26 +108,6 @@ def dn(x):
   rmdirie(x['root'])
   # save(x['cfg_path'], x['original_setup_cfg_content'])
   # save(x['py_path'], x['original_setup_py_content'])
-
-def f(x):
-  x = deepcopy(x)
-  z = {}
-  z['v'] = x['v'] if 'v' in x else None
-  z['root'] = x['root'] if 'root' in x else '.'
-  z['cfg_path'] = f'{z["root"]}/setup.cfg'
-  z['py_path'] = f'{z["root"]}/setup.py'
-
-  z['v'] = z['v'] or get_pip_version('hak')
-  z['v']['patch'] += 1
-
-  update_setup_cfg({'v': z['v'], 'filename': z['cfg_path']})
-  update_setup_py({'v': z['v'], 'filename': z['py_path']})
-  empty_directory('./dist/')
-  
-  make_new_dist_tar(x)
-  # add_to_git(cwd=_root, cap_out=True)
-  if 'local_test_only' not in x: z['upload_result'] = start_upload(x)
-  return z
 
 def t():
   x = up()
