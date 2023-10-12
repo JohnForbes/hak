499845c4043e6172584499a2f79791b4ce54e53e
John Forbes <john.robert.forbes@gmail.com>
2023-10-12 15:35:38 +1100

    Updated hak/ord.py.

--- hak/ord.py
+++ hak/ord.py
@@ -1,9 +1,16 @@
 from hak.pxyz import f as pxyz
 from hak.rate import Rate as R
 
+class ORD(R):
+  def __init__(self, numerator=0, denominator=1):
+    super().__init__(numerator, denominator, unit={'ORD': 1})
+
 # ORD
-f = lambda numerator=0, denominator=1: R(numerator, denominator, {'ORD': 1})
+f = lambda x: ORD(**x)
+# f = lambda numerator=0, denominator=1: R(numerator, denominator, {'ORD': 1})
 
 def t():
   x = {'numerator': 1, 'denominator': 2}
-  return pxyz(x, R(1, 2, {'ORD': 1}), f(**x))
+  y = R(1, 2, {'ORD': 1}).to_dict()
+  z = f(x).to_dict()
+  return pxyz(x, y, z)
