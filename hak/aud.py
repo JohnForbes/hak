9c28d3d3aef80a19db023e1aaf2a5edc7d64aae3
John Forbes <john.robert.forbes@gmail.com>
2023-10-12 15:51:49 +1100

    Updated hak/aud.py.

--- hak/aud.py
+++ hak/aud.py
@@ -4,6 +4,14 @@ from hak.pxyz import f as pxyz
 class AUD(R):
   def __init__(self, numerator=0, denominator=1):
     super().__init__(numerator, denominator, unit={'AUD': 1})
+  
+  def __add__(u, v):
+    if isinstance(v, AUD):
+      return AUD(u.n * v.d + v.n * u.d, u.d * v.d, u.unit)
+    elif isinstance(v, (int, float)):
+      return u + AUD(v, 1, u.unit)
+    else:
+      raise TypeError('Unsupported operand type for +')
 
   # __str__ = lambda s: (
   #   f'AUD({s.numerator})'
