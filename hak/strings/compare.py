2aee186026e07ec78802b0c7b7eb08f32b70f487
John Forbes <john.robert.forbes@gmail.com>
2023-10-17 12:22:30 +1100

    Updated hak/strings/compare.py.

--- hak/strings/compare.py
+++ hak/strings/compare.py
@@ -1,36 +1,70 @@
 from hak.puvyz import f as puvyz
 from hak.pf import f as pf
 
-def f(u, v):
-  if u == v: return (1, 'Match')
-  if len(u) < len(v): return (0, f'len(u): {len(u)} < len(v): {len(v)}')
-  if len(u) > len(v): return (0, f'len(u): {len(u)} > len(v): {len(v)}')
-  
-  for i in range(len(u)):
+def _calc_reason(u, v):
+  first_difference =  _get_first_difference(u, v)
+  return 'u == v' if u == v else (
+    f'len(u): {len(u)} < len(v): {len(v)}'
+    if len(u) < len(v) else
+    (
+      f'len(u): {len(u)} > len(v): {len(v)}'
+      if len(u) > len(v) else
+      ' != '.join([
+        f"x[{first_difference}]: '{u[first_difference]}'",
+        f"y[{first_difference}]: '{v[first_difference]}'"
+      ])
+    )
+  )
+
+def _get_first_difference(u, v):
+  _first_difference = min([len(u), len(v)])
+  for i in range(min([len(u), len(v)])):
     if u[i] != v[i]:
-      return (0, f"x[{i}]: '{u[i]}' != y[{i}]: '{v[i]}'", i)
-  
-  return (0, "Unknown mismatch")
+      return i
+  return _first_difference
+
+f = lambda u, v: {
+  'match': u == v,
+  'reason': _calc_reason(u, v),
+  'first_difference': _get_first_difference(u, v)
+}
   
 def t_match():
   u = 'abc'
   v = 'abc'
-  return puvyz(u, v, (1, 'Match'), f(u, v))
+  y = {'match': True, 'reason': 'u == v', 'first_difference': 3}
+  z = f(u, v)
+  return puvyz(u, v, y, z)
 
 def t_less():
   u = 'abc'
   v = 'abcd'
-  return puvyz(u, v, (0, 'len(u): 3 < len(v): 4'), f(u, v))
+  y = {
+    'match': False,
+    'reason': 'len(u): 3 < len(v): 4',
+    'first_difference': 3
+  }
+  return puvyz(u, v, y, f(u, v))
 
 def t_greater():
   u = 'abcd'
   v = 'abc'
-  return puvyz(u, v, (0, 'len(u): 4 > len(v): 3'), f(u, v))
+  y = {
+    'match': False,
+    'reason': 'len(u): 4 > len(v): 3',
+    'first_difference': 3
+  }
+  return puvyz(u, v, y, f(u, v))
 
 def t_not_equal():
   u = 'axc'
   v = 'ayc'
-  return puvyz(u, v, (0, "x[1]: 'x' != y[1]: 'y'", 1), f(u, v))
+  y = {
+    'match': False,
+    'reason': "x[1]: 'x' != y[1]: 'y'",
+    'first_difference': 1
+  }
+  return puvyz(u, v, y, f(u, v))
 
 def t():
   if not t_match(): return pf('!t_match')
