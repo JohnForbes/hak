8aef23bc9f49db8865edd18e90e2676c8b39b951
John Forbes <john.robert.forbes@gmail.com>
2023-10-18 15:50:08 +1100

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = Path("./README.md").read_text()
 
 setup(
   name='hak',
-  version='0.0.188',
+  version='0.0.189',
   license='MIT',
   description='Function Test Pair Toolbox',
   long_description=long_description,
